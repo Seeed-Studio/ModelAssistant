f30b065c4ed3e18bba287fcc4758f0f2db7321ca
jqm <jinqing.mu@seeed.cc>
2022-10-21 16:43:13 +0800

    add audio classify head

---
+++ models/heads/__init__.py
@@ -0,0 +1,4 @@
+from .cls_head import Audio_head
+from .taggregate_head import TAggregate
+
+__all__ = ['Audio_head', 'TAggregate']
---
+++ models/heads/cls_head.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+from mmcls.models.builder import HEADS
+
+
+@HEADS.register_module()
+class Audio_head(nn.Module):
+    def __init__(self, in_channels, n_classes, drop=0.5):
+        super(Audio_head, self).__init__()
+        self.avg = nn.AdaptiveAvgPool1d(1)
+        self.fc = nn.Linear(in_channels, in_channels)
+        self.fc1 = nn.Linear(in_channels, n_classes)
+        self.dp = nn.Dropout(drop)
+
+    def forward(self, x):
+        return self.fc1(self.dp(self.fc(self.avg(x).flatten(1))))
