b46d2d11b27adc1e19454ba6f44f95cbf6a68719
jqm <jinqing.mu@seeed.cc>
2022-11-05 19:01:45 +0800

    fix conda install

--- tools/env_config.py
+++ tools/env_config.py
@@ -3,14 +3,44 @@ import os
 import re
 import sys
 import time
-from subprocess import Popen,PIPE
+import logging
+import argparse
+
+from subprocess import Popen, PIPE
 from ubuntu_utils import cmd_result, ensure_base_env, get_job
 
-download_links={'miniconda':'https://repo.anaconda.com/miniconda/Miniconda3-py38_4.12.0-Linux-x86_64.sh','anaconda':'https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh'}
+download_links = {'miniconda': 'https://repo.anaconda.com/miniconda/Miniconda3-py38_4.12.0-Linux-x86_64.sh',
+                  'anaconda': 'https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh'}
+
+
+def log_init():
+    loger = logging.getLogger('ENV_CONFIG')
+    loger.setLevel(logging.INFO)
+
+    hd = logging.StreamHandler()
+    hd.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    hd.setFormatter(formatter)
+    loger.addHandler(hd)
+    return loger
+
+
+def command(cmd, retry_num=3):
+    for i in range(retry_num):
+        if os.system(cmd) != 0:
+            time.sleep(1)
+            loger.warning(f'COMMAND:"{cmd}"execution failed')
+            loger.info('retrying') if i != (retry_num-1) else None
+        else:
+            return True
+    return False
+
 
 def cmd_result(txt: str):
     cmd = os.popen(txt)
     return cmd.read().rstrip().lstrip()
+
 
 def get_job(argv) -> int:
     # get nprocs, if user not specified, use max(1, nproc-2)
@@ -28,19 +58,20 @@ def get_job(argv) -> int:
 
 
 def test_delay(name):
-    p = Popen(f'ping {name} -c 3',shell=True,encoding='utf8',stdout=PIPE)
+    p = Popen(f'ping {name} -c 3', shell=True, encoding='utf8', stdout=PIPE)
     data = p.stdout.read()
     p.wait(5)
     if len(data):
-        delay = re.findall('time=(.*?) ms',data)
+        delay = re.findall('time=(.*?) ms', data)
         print((sum([float(i) for i in delay])/len(delay)).__round__(2))
         return sum([float(i) for i in delay])/len(delay).__round__(2)
 
 
 def qure_ip():
-    p=subprocess.Popen('curl http://myip.ipip.net',shell=True,encoding='utf8',stdout=subprocess.PIPE)
+    p = subprocess.Popen('curl http://myip.ipip.net',
+                         shell=True, encoding='utf8', stdout=subprocess.PIPE)
     data = p.stdout.read()
-    print(data)
+    loger.info(data)
     if '中国' in data:
         return True
     return False
@@ -50,12 +81,13 @@ def test_network():
     delay = 1000
     domain = 'mirrors.aliyun.com'
     if qure_ip():
-        mirror={'pypi.mirrors.ustc.edu.cn':'https://pypi.mirrors.ustc.edu.cn/simple','pypi.douban.com':'http://pypi.douban.com/simple/','mirrors.aliyun.com':'http://mirrors.aliyun.com/pypi/simple/','pypi.tuna.tsinghua.edu.cn':'https://pypi.tuna.tsinghua.edu.cn/simple'}
+        mirror = {'pypi.mirrors.ustc.edu.cn': 'https://pypi.mirrors.ustc.edu.cn/simple', 'pypi.douban.com': 'http://pypi.douban.com/simple/',
+                  'mirrors.aliyun.com': 'http://mirrors.aliyun.com/pypi/simple/', 'pypi.tuna.tsinghua.edu.cn': 'https://pypi.tuna.tsinghua.edu.cn/simple'}
         for i in mirror.keys():
             test = test_delay(i)
             if delay > test:
-                delay=test
-                domain=i
+                delay = test
+                domain = i
         print(mirror[domain])
         return mirror[domain]
 
@@ -66,130 +98,165 @@ def test_network():
 
 
 def qure_gpu():
-    p=Popen('nvidia-smi |grep Driver',shell=True,encoding='utf8',stdout=PIPE)
+    p = Popen('nvidia-smi |grep Driver', shell=True,
+              encoding='utf8', stdout=PIPE)
     data = p.stdout.read()
     print(data)
     if len(data):
-        gpu = re.findall('NVIDIA-SMI (.*?) ',data)[0]
+        gpu = re.findall('NVIDIA-SMI (.*?) ', data)[0]
         return gpu
 
-    print('GPU:',data)
+    print('GPU:', data)
     print(p.returncode)
     return False
 
-GPU = qure_gpu()
 
-
-def download_file(link,path):
+def download_file(link, path):
     os.chdir(path)
-    code = os.system(f'wget {link}')
-    return code
+    if os.path.exists(os.path.join(path, link.split('/')[-1])):
+        loger.info(
+            f"{link.split('/')[-1]} already exists under the {path} path")
+        return
+    if command(f'wget {link}'):
+        loger.info(f"{link.split('/')[-1]} Downloaded")
+    else:
+        loger.warning(f"{link.split('/')[-1]} download failed")
 
 
-def write_command(command,path):
-    open(path,'w',encoding='utf8').write(command)
+def write_command(command, path):
+    open(path, 'w', encoding='utf8').write(command)
 
 
-def anaconda_install(now_path):
+def anaconda_install(now_path, conda='miniconda'):
     os.chdir(now_path)
-    os.makedirs('tmp',exist_ok=True)
-    # download_file(download_links['miniconda'], now_path)
-    os.system('chmod +x ./Miniconda3-py38_4.12.0-Linux-x86_64.sh')
-    write_command('\nq\n\nyes\n\nyes\n','./tmp/input.tmp')
-    time.sleep(1)
-    f = open('/home/dq/software/tmp/input.tmp','r',encoding='utf8')
+    os.makedirs('tmp', exist_ok=True)
+    if command('conda -V'):
+        loger.info(
+            'Your conda has been installed, skip the installation this time')
+        return
 
-    r=subprocess.Popen(args='/home/dq/software/Miniconda3-py38_4.12.0-Linux-x86_64.sh',stdin=subprocess.PIPE,stdout=None,shell=True,encoding='utf8') 
+    file_name = download_links[conda].split('/')[-1]
+    download_file(download_links[conda], now_path)
+
+    r = subprocess.Popen(args=f'./{file_name}', stdin=PIPE,
+                         stderr=PIPE, stdout=None, shell=True, encoding='utf8')
     try:
-        r.communicate('\nq\n\nyes\n\nyes\n')
-        r.stdin.flush()
+        r.communicate('\nq\n\nyes\n\nyes\n\n')
         # r.wait(15)
     except:
         r.kill()
-    
-
+        loger.info('')
 
 
 def cuda_install(now_path):
     os.chdir(now_path)
     if not os.path.exists('./cuda_11.2.1_460.32.03_linux.run'):
-        os.system('wget https://developer.download.nvidia.com/compute/cuda/11.2.1/local_installers/cuda_11.2.1_460.32.03_linux.run')
+        command(
+            'wget https://developer.download.nvidia.com/compute/cuda/11.2.1/local_installers/cuda_11.2.1_460.32.03_linux.run')
 
-    os.system('wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin')
-    os.system('sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600')
-    os.system('wget https://developer.download.nvidia.com/compute/cuda/11.2.1/local_installers/cuda-repo-ubuntu2004-11-2-local_11.2.1-460.32.03-1_amd64.deb')
-    os.system('sudo dpkg -i cuda-repo-ubuntu2004-11-2-local_11.2.1-460.32.03-1_amd64.deb')
-    os.system('sudo apt-key add /var/cuda-repo-ubuntu2004-11-2-local/7fa2af80.pub')
-    os.system('sudo apt-get update && sudo apt-get -y install cuda')
-
-
-def mmlab_install(now_path):
-    os.system('pip install mmdeploy mmdet mmcls mmpose openmim ' )
-    os.system('pip install mmcv-full  -f https://download.openmmlab.com/mmcv/dist/cu116/torch1.13.0/index.html')
-    print('mmlab env install succeeded!')
+    command('wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin')
+    command(
+        'sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600')
+    command('wget https://developer.download.nvidia.com/compute/cuda/11.2.1/local_installers/cuda-repo-ubuntu2004-11-2-local_11.2.1-460.32.03-1_amd64.deb')
+    command(
+        'sudo dpkg -i cuda-repo-ubuntu2004-11-2-local_11.2.1-460.32.03-1_amd64.deb')
+    command('sudo apt-key add /var/cuda-repo-ubuntu2004-11-2-local/7fa2af80.pub')
+    command('sudo apt-get update && sudo apt-get -y install cuda')
 
 
-def torch_install(now_path):
-    p = subprocess.Popen(args='pip list | grep torch',shell=True,encoding='utf8',stdout=subprocess.PIPE,stdin=subprocess.PIPE)
+def mmlab_install(env_name, conda_name):
+    if command(f'{pip} install -r {req_path}/requirements.txt'):
+        loger.info('mmlab env install succeeded!')
+    if command(
+            '{pip} install mmcv-full  -f https://download.openmmlab.com/mmcv/dist/cu113/torch1.10.0/index.html'):
+        loger.info('mmcv-full install succeeded!')
+
+
+def torch_install(env_name, conda_name):
+    if not command(f'{pip} list'):
+        loger.warning(f'Environment {env_name} query failed')
+        return
+
+    p = subprocess.Popen(args=f'{pip} list | grep torch', shell=True,
+                         encoding='utf8', stdout=subprocess.PIPE, stdin=subprocess.PIPE)
     data = p.stdout.read()
-    if len(data) and 'torch ' in data :
-        print('Torch is has installed!')
-        # return
-    
+    if len(data) and 'torch ' in data:
+        loger.warning('Torch is has installed!')
+        return
+
     if GPU:
         # p=subprocess.Popen(args='nvcc --version | grep release',stdout=subprocess.PIPE,encoding='utf8',shell=True)
-        p=subprocess.Popen(args='nvidia-smi | grep Driver',stdout=subprocess.PIPE,encoding='utf8',shell=True)
+        p = subprocess.Popen(args='nvidia-smi | grep Driver',
+                             stdout=subprocess.PIPE, encoding='utf8', shell=True)
         data = p.stdout.read()
         if len(data) and 'Driver' in data:
-            version = re.findall('CUDA Version: (.*?) ',data)[0]
-            print('CUDA VERSION:', version)
+            version = re.findall('CUDA Version: (.*?) ', data)[0]
+            loger.info(f'CUDA VERSION:{version}')
         else:
-            print('未查询到cuda版本！')
-        os.system(f'pip install torch==1.10.0 torchvision==0.11.1 torchaudio==0.10.0 --extra-index-url https://download.pytorch.org/whl/cu{version.replace(".","")}')
+            loger.info('未查询到cuda版本！')
+        command(
+            f'{pip} install torch==1.10.0+cu113 torchvision==0.11.1+cu113 torchaudio==0.10.0+cu113 --extra-index-url https://download.pytorch.org/whl/cu113')
     else:
-        os.system('pip install torch torchvision torchaudio')
-
-
-def ncnn_install(now_path):
-    pass
+        command(f'{pip} install torch torchvision torchaudio')
 
 
 def conda_acti_env(name):
-    os.system(f'conda activate {name}')
+    command('conda info -e')
+    time.sleep(2)
+    if command(f'~/anaconda3/bin/conda activate {name}'):
+        loger.info(f'The virtual environment {name} is activated')
+    else:
+        loger.warning(f'Virtual environment {name} activation failed')
 
 
-def conda_create_env(name,version=3.8):
-    p= subprocess.Popen(f'conda create -n {name} python=={version}',stdin=subprocess.PIPE,shell=True,encoding='utf8')
+def conda_create_env(name, version=3.8):
+    p = subprocess.Popen(
+        f'conda info -e', stdout=subprocess.PIPE, shell=True, encoding='utf8')
+    if name in p.stdout.read():
+        loger.info(f'The virtual environment {name} already exists')
+        return
+
+    p = subprocess.Popen(
+        f'conda create -n {name} python=={version}', stdin=subprocess.PIPE, shell=True, encoding='utf8')
     p.communicate('y\n')
 
-g_jobs=16
+    p = subprocess.Popen(
+        f'conda info -e', stdout=subprocess.PIPE, shell=True, encoding='utf8')
+    if name in p.stdout.read():
+        loger.info(f'The virtual environment {name} has been created')
+    else:
+        loger.warning(f'Failed to create virtual environment {name}')
+
+
+g_jobs = 16
+
 
 def install_protobuf(dep_dir) -> int:
     print('-' * 10 + 'install protobuf' + '-' * 10)
 
     os.chdir(dep_dir)
     if not os.path.exists('protobuf-3.20.0'):
-        os.system(
+        command(
             'wget https://github.com/protocolbuffers/protobuf/releases/download/v3.20.0/protobuf-cpp-3.20.0.tar.gz'  # noqa: E501
         )
-        os.system('tar xvf protobuf-cpp-3.20.0.tar.gz')
+        command('tar xvf protobuf-cpp-3.20.0.tar.gz')
 
     os.chdir(os.path.join(dep_dir, 'protobuf-3.20.0'))
 
     install_dir = os.path.join(dep_dir, 'pbinstall')
     if os.path.exists(install_dir):
-        os.system('rm -rf {}'.format(install_dir))
+        command('rm -rf {}'.format(install_dir))
 
-    os.system('make clean')
-    os.system('./configure --prefix={}'.format(install_dir))
-    os.system('make -j {} && make install'.format(g_jobs))
+    command('make clean')
+    command('./configure --prefix={}'.format(install_dir))
+    command('make -j {} && make install'.format(g_jobs))
     protoc = os.path.join(install_dir, 'bin', 'protoc')
 
     print('protoc \t:{}'.format(cmd_result('{} --version'.format(protoc))))
 
-    os.system(""" echo 'export PATH={}:$PATH' >> ~/mmdeploy.env """.format(
+    command(""" echo 'export PATH={}:$PATH' >> ~/mmdeploy.env """.format(
         os.path.join(install_dir, 'bin')))
-    os.system(
+    command(
         """ echo 'export LD_LIBRARY_PATH={}:$LD_LIBRARY_PATH' >> ~/mmdeploy.env """  # noqa: E501
         .format(os.path.join(install_dir, 'lib')))
 
@@ -205,7 +272,7 @@ def install_pyncnn(dep_dir):
 
     # git clone
     if not os.path.exists('ncnn'):
-        os.system(
+        command(
             'git clone --depth 1 --branch 20220729  https://github.com/tencent/ncnn && cd ncnn'  # noqa: E501
         )
 
@@ -213,13 +280,13 @@ def install_pyncnn(dep_dir):
     os.chdir(ncnn_dir)
 
     # update submodule pybind11, gslang not required
-    os.system('git submodule init && git submodule update python/pybind11')
+    command('git submodule init && git submodule update python/pybind11')
     # build
     if not os.path.exists('build'):
-        os.system('mkdir build')
+        command('mkdir build')
 
     os.chdir(os.path.join(ncnn_dir, 'build'))
-    os.system('rm -rf CMakeCache.txt')
+    command('rm -rf CMakeCache.txt')
     pb_install = os.path.join(dep_dir, 'pbinstall')
     pb_bin = os.path.join(pb_install, 'bin', 'protoc')
     pb_lib = os.path.join(pb_install, 'lib', 'libprotobuf.so')
@@ -232,25 +299,25 @@ def install_pyncnn(dep_dir):
     cmd += ' -DProtobuf_INCLUDE_DIR={} '.format(pb_include)
     cmd += ' && make -j {} '.format(g_jobs)
     cmd += ' && make install '
-    os.system(cmd)
+    command(cmd)
 
     # install
     os.chdir(ncnn_dir)
-    os.system('cd python && pip install -e .')
+    command(f'cd python && {pip} install -e .')
     ncnn_cmake_dir = os.path.join(ncnn_dir, 'build', 'install', 'lib', 'cmake',
                                   'ncnn')
-    onnx_path = os.path.join(ncnn_dir,'build','tools','bin','onnx')
-    quan_path = os.path.join(ncnn_dir,'build','tools','bin','quantize')
-    caffe_path = os.path.join(ncnn_dir,'build','tools','bin','caffe')
-    os.system(f'echo export PATH={onnx_path}:\$PATH >> ~/.bashrc')
-    os.system(f'echo export PATH={quan_path}:\$PATH >> ~/.bashrc')
-    os.system(f'echo export PATH={caffe_path}:\$PATH >> ~/.bashrc')
+    onnx_path = os.path.join(ncnn_dir, 'build', 'tools', 'bin', 'onnx')
+    quan_path = os.path.join(ncnn_dir, 'build', 'tools', 'bin', 'quantize')
+    caffe_path = os.path.join(ncnn_dir, 'build', 'tools', 'bin', 'caffe')
+    command(f'echo export PATH={onnx_path}:\$PATH >> ~/.bashrc')
+    command(f'echo export PATH={quan_path}:\$PATH >> ~/.bashrc')
+    command(f'echo export PATH={caffe_path}:\$PATH >> ~/.bashrc')
 
-    os.makedirs(ncnn_cmake_dir,exist_ok=True)
+    os.makedirs(ncnn_cmake_dir, exist_ok=True)
     assert (os.path.exists(ncnn_cmake_dir))
-    print('ncnn cmake dir \t:{}'.format(ncnn_cmake_dir))
-    print('\n')
+    loger.info('ncnn cmake dir \t:{}'.format(ncnn_cmake_dir))
     return ncnn_cmake_dir
+
 
 def main():
     """Auto install mmdeploy with ncnn. To verify this script:
@@ -271,7 +338,8 @@ def main():
     dep_dir = '/home/dq/software'
     if not os.path.exists(dep_dir):
         if os.path.isfile(dep_dir):
-            print('{} already exists and it is a file, exit.'.format(work_dir))
+            loger.info(
+                '{} already exists and it is a file, exit.'.format(work_dir))
             return -1
         os.mkdir(dep_dir)
 
@@ -279,32 +347,40 @@ def main():
     if success != 0:
         return -1
 
-    # if install_protobuf(dep_dir) != 0:
-        # return -1
+    if install_protobuf(dep_dir) != 0:
+        return -1
 
     ncnn_cmake_dir = install_pyncnn(dep_dir)
 
-if __name__=='__main__':
-    try:
-        anaconda_install('/home/dq/software')
-        time.sleep(30)
-        # os.system('exec bash && source ~/.bashrc')
-        conda_create_env('edgelab')
-        conda_acti_env('edgelab')
 
-        os.system('conda info -e')
-        qure_gpu()
-
-        # # cuda_install('/home/dq/software')
-        torch_install('/home/dq/software')
-        mmlab_install('/home/dq/software')
-        conda_acti_env('edgelab')
-        # main()
-        # os.system('exec bash && source ~/.bashrc')
-        conda_acti_env('edgelab')
-        
-        install_protobuf('/home/dq/software')
-        install_pyncnn('/home/dq/software')
-    except:pass
+def pare_args():
+    args = argparse.ArgumentParser()
+    args.add_argument('--action', default='')
+    args.add_argument('--envname', default='edgelab3',
+                      help='conda vertual enverimen name')
+    args.add_argument('--conda', default='miniconda',
+                      help='conda vertual enverimen name')
+    args.add_argument('--force', action='store_true',
+                      help='wether force install')
+    return args.parse_args()
 
 
+if __name__ == '__main__':
+    now_path = '/home/dq/software'
+    args = pare_args()
+    loger = log_init()
+    GPU = qure_gpu()
+    pip = f'~/{args.conda}3/envs/{args.envname}/bin/pip'
+
+    anaconda_install(now_path, conda=args.conda)
+    conda_create_env(args.envname)
+
+    # # cuda_install(now_path)
+    torch_install(args.envname, args.conda)
+    self_dir = os.path.abspath(__file__)
+    req_path = os.path.join(os.path.dirname(self_dir), '..', 'requirements')
+
+    mmlab_install(args.envname, args.conda)
+
+    install_protobuf(now_path)
+    install_pyncnn(now_path)
