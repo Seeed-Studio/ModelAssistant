1346c450f8785d242fafe30c50a6de1391979f16
mjq2020 <mjqx2011@163.com>
2022-11-17 09:05:34 +0000

    Optimized installation

--- tools/env_config.py
+++ tools/env_config.py
@@ -1,13 +1,13 @@
-import subprocess
 import os
 import re
 import time
 import logging
 import argparse
+import subprocess
 import os.path as osp
 
 from subprocess import Popen, PIPE
-from ubuntu_utils import cmd_result, ensure_base_env, get_job
+from ubuntu_utils import cmd_result, ensure_base_env
 
 download_links = {
     'miniconda':
@@ -41,26 +41,6 @@ def command(cmd, retry_num=3):
     return False
 
 
-def cmd_result(txt: str):
-    cmd = os.popen(txt)
-    return cmd.read().rstrip().lstrip()
-
-
-def get_job(argv) -> int:
-    # get nprocs, if user not specified, use max(1, nproc-2)
-    job = 2
-    if len(argv) <= 1:
-        print('your can use `python3 {} N` to set make -j [N]'.format(argv[0]))
-        nproc = cmd_result('nproc')
-        if nproc is not None and len(nproc) > 0:
-            job = max(int(nproc) - 2, 1)
-        else:
-            job = 1
-    else:
-        job = int(argv[1])
-    return job
-
-
 def test_delay(name):
     p = Popen(f'ping {name} -c 3', shell=True, encoding='utf8', stdout=PIPE)
     loger.info(f'testing to "{name}" delay')
@@ -68,11 +48,8 @@ def test_delay(name):
     p.wait(5)
     if len(data):
         delay = re.findall('time=(.*?) ms', data)
-        if len(delay):
-            print((sum([float(i) for i in delay]) / len(delay)).__round__(2))
-            return sum([float(i) for i in delay]) / len(delay).__round__(2)
-        else:
-            return 1000
+        return sum([float(i) for i in delay]) / len(delay).__round__(2) if len(
+            delay) else 1000
 
 
 def qure_ip():
@@ -119,7 +96,7 @@ def qure_gpu():
 
 def download_file(link, path):
     os.chdir(path)
-    if os.path.exists(os.path.join(path, link.split('/')[-1])):
+    if osp.exists(osp.join(path, link.split('/')[-1])):
         loger.info(
             f"{link.split('/')[-1]} already exists under the {path} path")
         return
@@ -129,20 +106,16 @@ def download_file(link, path):
         loger.warning(f"{link.split('/')[-1]} download failed")
 
 
-def write_command(command, path):
-    open(path, 'w', encoding='utf8').write(command)
-
-
-def anaconda_install(now_path, conda='miniconda'):
-    os.chdir(now_path)
-    os.makedirs('tmp', exist_ok=True)
+def anaconda_install(conda='miniconda'):
+    os.chdir(proce_path)
     if command(f'{conda_bin} -V'):
         loger.info(
             'Your conda has been installed, skip the installation this time')
         return
 
     file_name = download_links[conda].split('/')[-1]
-    download_file(download_links[conda], now_path)
+    if not osp.exists(file_name):
+        download_file(download_links[conda], proce_path)
 
     time.sleep(1)
     command(f'chmod +x {file_name}')
@@ -160,9 +133,9 @@ def anaconda_install(now_path, conda='miniconda'):
         loger.info('anaconda installation failed!')
 
 
-def cuda_install(now_path):
-    os.chdir(now_path)
-    if not os.path.exists('./cuda_11.2.1_460.32.03_linux.run'):
+def cuda_install():
+    os.chdir(proce_path)
+    if not osp.exists('./cuda_11.2.1_460.32.03_linux.run'):
         command(
             'wget https://developer.download.nvidia.com/compute/cuda/11.2.1/local_installers/cuda_11.2.1_460.32.03_linux.run'
         )
@@ -230,7 +203,6 @@ def torch_install():
         return
 
     if GPU:
-        # p=subprocess.Popen(args='nvcc --version | grep release',stdout=subprocess.PIPE,encoding='utf8',shell=True)
         p = subprocess.Popen(args='nvidia-smi | grep Driver',
                              stdout=subprocess.PIPE,
                              encoding='utf8',
@@ -251,8 +223,7 @@ def torch_install():
 
 
 def mmlab_install():
-    self_dir = os.path.abspath(__file__)
-    req_path = os.path.join(os.path.dirname(self_dir), '..', 'requirements')
+    req_path = osp.join(project_path, 'requirements')
     if command(f'{pip} install -r {req_path}/requirements.txt' + pip_mirror):
         loger.info('mmlab env install succeeded!')
     if GPU:
@@ -265,20 +236,20 @@ def mmlab_install():
         loger.info('mmcv-full install succeeded!')
 
 
-def install_protobuf(dep_dir) -> int:
+def install_protobuf() -> int:
     print('-' * 10 + 'install protobuf' + '-' * 10)
 
-    os.chdir(dep_dir)
-    if not os.path.exists('protobuf-cpp-3.20.0.tar.gz'):
+    os.chdir(proce_path)
+    if not osp.exists('protobuf-cpp-3.20.0.tar.gz'):
         command(
             'wget https://github.com/protocolbuffers/protobuf/releases/download/v3.20.0/protobuf-cpp-3.20.0.tar.gz'  # noqa: E501
         )
         command('tar xvf protobuf-cpp-3.20.0.tar.gz')
 
-    os.chdir(os.path.join(dep_dir, 'protobuf-3.20.0'))
+    os.chdir(osp.join(proce_path, 'protobuf-3.20.0'))
 
-    install_dir = os.path.join(dep_dir, 'pbinstall')
-    if os.path.exists(install_dir):
+    install_dir = osp.join(proce_path, 'pbinstall')
+    if osp.exists(install_dir):
         command('rm -rf {}'.format(install_dir))
     else:
         os.makedirs(install_dir, exist_ok=True)
@@ -286,47 +257,47 @@ def install_protobuf(dep_dir) -> int:
     command('make clean')
     command('./configure --prefix={}'.format(install_dir))
     command('make -j {} && make install'.format(g_jobs))
-    protoc = os.path.join(install_dir, 'bin', 'protoc')
+    protoc = osp.join(install_dir, 'bin', 'protoc')
 
     print('protoc \t:{}'.format(cmd_result('{} --version'.format(protoc))))
 
     command(""" echo 'export PATH={}:$PATH' >> ~/mmdeploy.env """.format(
-        os.path.join(install_dir, 'bin')))
+        osp.join(install_dir, 'bin')))
     command(
         """ echo 'export LD_LIBRARY_PATH={}:$LD_LIBRARY_PATH' >> ~/mmdeploy.env """  # noqa: E501
-        .format(os.path.join(install_dir, 'lib')))
+        .format(osp.join(install_dir, 'lib')))
 
     return 0
 
 
-def install_pyncnn(dep_dir):
+def install_pyncnn():
     print('-' * 10 + 'build and install pyncnn' + '-' * 10)
     time.sleep(2)
 
     # generate unzip and build dir
-    os.chdir(dep_dir)
+    os.chdir(proce_path)
 
     # git clone
-    if not os.path.exists('ncnn'):
+    if not osp.exists('ncnn'):
         command(
             'git clone --depth 1 --branch 20220729  https://github.com/tencent/ncnn && cd ncnn'  # noqa: E501
         )
 
-    ncnn_dir = os.path.join(dep_dir, 'ncnn')
+    ncnn_dir = osp.join(proce_path, 'ncnn')
     os.chdir(ncnn_dir)
 
     # update submodule pybind11, gslang not required
     command('git submodule init && git submodule update python/pybind11')
     # build
-    if not os.path.exists('build'):
+    if not osp.exists('build'):
         command('mkdir build')
 
-    os.chdir(os.path.join(ncnn_dir, 'build'))
+    os.chdir(osp.join(ncnn_dir, 'build'))
     command('rm -rf CMakeCache.txt')
-    pb_install = os.path.join(dep_dir, 'pbinstall')
-    pb_bin = os.path.join(pb_install, 'bin', 'protoc')
-    pb_lib = os.path.join(pb_install, 'lib', 'libprotobuf.so')
-    pb_include = os.path.join(pb_install, 'include')
+    pb_install = osp.join(proce_path, 'pbinstall')
+    pb_bin = osp.join(pb_install, 'bin', 'protoc')
+    pb_lib = osp.join(pb_install, 'lib', 'libprotobuf.so')
+    pb_include = osp.join(pb_install, 'include')
 
     cmd = 'cmake .. '
     cmd += ' -DNCNN_PYTHON=ON '
@@ -340,22 +311,20 @@ def install_pyncnn(dep_dir):
     # install
     os.chdir(ncnn_dir)
     command(f'cd python && {pip} install -e .' + pip_mirror)
-    ncnn_cmake_dir = os.path.join(ncnn_dir, 'build', 'install', 'lib', 'cmake',
-                                  'ncnn')
-    onnx_path = os.path.join(ncnn_dir, 'build', 'tools', 'onnx')
-    quan_path = os.path.join(ncnn_dir, 'build', 'tools', 'quantize')
-    caffe_path = os.path.join(ncnn_dir, 'build', 'tools', 'caffe')
-    command(f'echo export PATH={onnx_path}:\$PATH >> ~/.bashrc'
-            ) if os.path.exists(onnx_path) else None
-    command(f'echo export PATH={quan_path}:\$PATH >> ~/.bashrc'
-            ) if os.path.exists(quan_path) else None
-    command(f'echo export PATH={caffe_path}:\$PATH >> ~/.bashrc'
-            ) if os.path.exists(caffe_path) else None
 
-    os.makedirs(ncnn_cmake_dir, exist_ok=True)
-    assert (os.path.exists(ncnn_cmake_dir))
-    loger.info('ncnn cmake dir \t:{}'.format(ncnn_cmake_dir))
-    return ncnn_cmake_dir
+    path_ls = []
+    path_ls.append(osp.join(ncnn_dir, 'build', 'tools', 'onnx'))
+    path_ls.append(osp.join(ncnn_dir, 'build', 'tools', 'quantize'))
+    path_ls.append(osp.join(ncnn_dir, 'build', 'tools', 'caffe'))
+    path_ls.append(osp.join(ncnn_dir, 'build', 'install', 'bin'))
+    path_ls.append(osp.join(ncnn_dir, 'build', 'install', 'include'))
+    path_ls.append(osp.join(ncnn_dir, 'build', 'install', 'lib'))
+    PATH = os.environ['PATH']
+    for p in path_ls:
+        if p in PATH: continue
+        else:
+            command(f'echo export PATH={p}:\$PATH >> ~/.bashrc') if osp.exists(
+                p) else None
 
 
 def proto_ncnn_install():
@@ -369,53 +338,38 @@ def proto_ncnn_install():
     Returns:
         _type_: _description_
     """
-
-
-    # dep_dir = os.path.abspath(os.path.join(work_dir, '..', 'mmdeploy-dep'))
-    if not os.path.exists(dep_dir):
-        if os.path.isfile(dep_dir):
-            loger.info(
-                '{} already exists and it is a file, exit.'.format(now_path))
-            return -1
-        os.mkdir(dep_dir)
-
-    success = ensure_base_env(now_path, dep_dir)
+    success = ensure_base_env(project_path, proce_path)
     if success != 0:
         return -1
 
-    if install_protobuf(dep_dir) != 0:
+    if install_protobuf() != 0:
         return -1
 
-    ncnn_cmake_dir = install_pyncnn(dep_dir)
+    install_pyncnn()
 
 
 def pare_args():
     args = argparse.ArgumentParser()
-    args.add_argument('--action', default='')
     args.add_argument('--envname',
-                      default='test1',
+                      default='edgelab',
                       help='conda vertual enverimen name')
     args.add_argument('--conda',
                       default='anaconda',
                       help='conda vertual enverimen name')
-    args.add_argument('--force',
-                      action='store_true',
-                      help='wether force install')
     return args.parse_args()
 
 
 def prepare():
-    global args, project_path, pip, conda_bin, home, now_path, dep_dir, loger, pip_mirror, GPU, g_jobs
+    global args, project_path, pip, conda_bin, home, proce_path, loger, pip_mirror, GPU, g_jobs
     args = pare_args()
     g_jobs = os.cpu_count() if os.cpu_count else 8
     project_path = osp.dirname(osp.dirname(osp.abspath(__file__)))
     pip = f'~/{args.conda}3/envs/{args.envname}/bin/pip'
     home = os.environ['HOME']
     conda_bin = f'{home}/{args.conda}3/bin/conda'
-    now_path = f'{home}/software'
-    dep_dir = os.path.abspath(os.path.join(now_path, '..', 'mmdeploy-dep'))
+    proce_path = f'{home}/software'
 
-    os.makedirs(now_path, exist_ok=True)
+    os.makedirs(proce_path, exist_ok=True)
     loger = log_init()
     mirror = test_network()
     pip_mirror = f' -i {mirror[0]} --trusted-host {mirror[1]}' if mirror else ''
@@ -424,12 +378,12 @@ def prepare():
 
 def main():
     prepare()
-    ensure_base_env(now_path, dep_dir)
+    ensure_base_env(proce_path, proce_path)
 
-    anaconda_install(now_path, conda=args.conda)
+    anaconda_install(conda=args.conda)
     conda_create_env(args.envname)
 
-    # cuda_install(now_path)
+    # cuda_install()
     torch_install()
     # mmlab
     mmlab_install()
