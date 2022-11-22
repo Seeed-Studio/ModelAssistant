133b3004c3ed6b4ec52866a21247f59c1c25bf17
mjq2020 <mjqx2011@163.com>
2022-11-22 08:31:43 +0000

    add decompress

--- datasets/utils/download.py
+++ datasets/utils/download.py
@@ -3,7 +3,33 @@ import platform
 from typing import List, AnyStr
 
 
-def download(links: List or AnyStr, store_path: AnyStr or __path__):
+def check_compress(file):
+    compress_tools = {
+        'tar': "tar",
+        "gz": "gunzip",
+        "tgz": "tar",
+        "zip": "unzip",
+        "rar": "unrar"
+    }
+    if 'tar.gz' in file: return ["tar -z"]
+    fls = file.split('.')
+    res = []
+    for f in fls[::-1]:
+        if f in compress_tools.keys():
+            res.append(compress_tools[f])
+    return res
+
+
+def defile(files, store_dir):
+    res = []
+    for f in file:
+        cmd = check_compress(f)
+        res.append(cmd)
+
+
+def download(links: List or AnyStr,
+             store_path: AnyStr or __path__,
+             unzip_dir=None):
     if isinstance(links, list):
         pass
     elif isinstance(links, str):
@@ -18,11 +44,15 @@ def download(links: List or AnyStr, store_path: AnyStr or __path__):
     print(store_path)
     for link in links:
         file_name = link.split('/')[-1]
+        unzip = f"tar -zxvf {file_name} -C ."
         if os.path.exists(file_name):
             print(f"file {file_name} already exists in {store_path}")
+            print(unzip)
+            os.system(unzip)
             continue
         cmd = f"curl -L {link} -o {file_name} --retry 3 -C -"
         os.system(cmd)
+        os.system(unzip)
 
 
 def check_file(path, store_dir=None, data_name=None):
@@ -32,8 +62,11 @@ def check_file(path, store_dir=None, data_name=None):
         download_dir = os.path.join(download_dir,
                                     data_name) if data_name else download_dir
         if not os.path.exists(download_dir):
-            os.makedirs(download_dir, exist_ok=True)
-        download(path, download_dir)
+            os.makedirs(download_dir, exist_ok=True)            # makedir the datasets
+
+        download(path, download_dir, data_name)
+
+
     else:
         download_dir = path
     return download_dir
