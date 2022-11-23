e317d6e5859a3a08d6cdb856088f035ddbee129e
mjq2020 <mjqx2011@163.com>
2022-11-23 06:53:31 +0000

    add custome dataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,5 +1,6 @@
 from .speechcommand import Speechcommand
 from .meter import MeterData
+from .cocodataset import CustomCocoDataset
 from .pipelines import *
 
-__all__ = ['Speechcommand', 'MeterData', 'AudioAugs']
+__all__ = ['Speechcommand', 'MeterData', 'AudioAugs', 'CustomCocoDataset']
