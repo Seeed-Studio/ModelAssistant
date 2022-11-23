cbc22244a93592ffc16ce52a18638b63c6641b91
mjq2020 <mjqx2011@163.com>
2022-11-23 06:37:58 +0000

    optimizer data decompress

--- datasets/utils/download.py
+++ datasets/utils/download.py
@@ -5,13 +5,13 @@ from typing import List, AnyStr
 
 def check_compress(file):
     compress_tools = {
-        'tar': "tar",
-        "gz": "gunzip",
-        "tgz": "tar",
-        "zip": "unzip",
+        'tar': "tar -xf {} -C ..",
+        "gz": "gunzip ",
+        "tgz": "tar -zxf {} -C ..",
+        "zip": "unzip -n {} -d ..",
         "rar": "unrar"
     }
-    if 'tar.gz' in file: return ["tar -z"]
+    if 'tar.gz' in file: return ["tar -zxf {} -C .."]
     fls = file.split('.')
     res = []
     for f in fls[::-1]:
@@ -30,33 +30,42 @@ def defile(files, store_dir):
 def download(links: List or AnyStr,
              store_path: AnyStr or __path__,
              unzip_dir=None):
-    if isinstance(links, list):
-        pass
-    elif isinstance(links, str):
-        links = [links]
-    else:
-        raise TypeError(
-            'The download link must be a list or a string, but got {} type'.
-            format(getattr(type(links), '__name__', repr(type(links)))))
-
     os.chdir(store_path)
+    if not os.path.exists('download'):
+        os.mkdir('download')
+    os.chdir('download')
+
     print(links)
     print(store_path)
     for link in links:
         file_name = link.split('/')[-1]
-        unzip = f"tar -zxvf {file_name} -C ."
+        unzip = check_compress(file_name)
+        unzip = [de.format(file_name) for de in unzip]
         if os.path.exists(file_name):
             print(f"file {file_name} already exists in {store_path}")
-            print(unzip)
-            os.system(unzip)
+            if not os.path.exists('.'+file_name):
+                for de in unzip:os.system(de)
+                os.system(f'touch .{file_name}')
             continue
+
+        # Dlownd compress dataset
         cmd = f"curl -L {link} -o {file_name} --retry 3 -C -"
         os.system(cmd)
-        os.system(unzip)
+        for de in unzip:os.system(de)
+        os.system(f'touch .{file_name}')
 
 
 def check_file(path, store_dir=None, data_name=None):
-    if 'https://' in path or 'http://' in path:
+    if isinstance(path, list):
+        pass
+    elif isinstance(path, str):
+        path = [path]
+    else:
+        raise TypeError(
+            'The download link must be a list or a string, but got {} type'.
+            format(getattr(type(path), '__name__', repr(type(path)))))
+
+    if 'https://' in path[0] or 'http://' in path[0]:
         download_dir = f"{os.environ['HOME']}/datasets" if platform.system(
         ) == 'Linux' and not store_dir else 'D:\datasets' if not store_dir else store_dir
         download_dir = os.path.join(download_dir,
