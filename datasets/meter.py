66a562c66441b59359303450241b798e87518a0c
mjq2020 <mjqx2011@163.com>
2022-11-25 06:43:32 +0000

    optimizer data loading

--- datasets/meter.py
+++ datasets/meter.py
@@ -1,13 +1,18 @@
+import os
 import math
-import numpy as np
+
 import cv2
-import albumentations as A
 import torch
+import numpy as np
+import albumentations as A
 
 from mmpose.datasets.builder import DATASETS
 from torch.utils.data import Dataset
 from torchvision import transforms
 from torchvision.transforms.functional import InterpolationMode
+
+from datasets.utils.download import check_file
+from datasets.pipelines.pose_transform import Pose_Compose
 
 
 def calc_angle(x1, y1, x2, y2):
@@ -15,7 +20,8 @@ def calc_angle(x1, y1, x2, y2):
     y = (y1 - y2)
     z = math.sqrt(x * x + y * y)
     try:
-        angle = math.acos((z ** 2 + 1 - (x - 1) ** 2 - y ** 2) / (2 * z * 1)) / math.pi * 180
+        angle = math.acos(
+            (z**2 + 1 - (x - 1)**2 - y**2) / (2 * z * 1)) / math.pi * 180
     except:
         angle = 0
 
@@ -29,66 +35,50 @@ def calc_angle(x1, y1, x2, y2):
 class MeterData(Dataset):
     CLASSES = ('meter')
 
-    def __init__(self, index_file, test_mode=None, transform=None):
+    def __init__(self,
+                 data_root,
+                 index_file,
+                 pipeline,
+                 format='xy',
+                 test_mode=None):
         super(MeterData, self).__init__()
 
-        self.train = transform
-        self.test_trans = transforms.Compose(
-            [transforms.ToTensor(), transforms.Resize(size=(112, 112), interpolation=InterpolationMode.NEAREST)])
-        self.transforms = A.Compose([
-            A.ColorJitter(brightness=0.3, p=0.5),
-            A.OneOf([
-                A.IAAAdditiveGaussianNoise(),
-                A.GaussNoise(),
-            ], p=0.5),
-            A.OneOf([
-                A.MotionBlur(p=0.3),
-                A.MedianBlur(blur_limit=3, p=0.3),
-                A.Blur(blur_limit=3, p=0.3),
-            ], p=0.5),
-            A.HorizontalFlip(),
-            A.VerticalFlip(),
-            A.Rotate(border_mode=cv2.BORDER_REPLICATE, interpolation=cv2.INTER_NEAREST,),
-            # A.ChannelShuffle(),
-            # A.SafeRotate(border_mode=cv2.BORDER_REPLICATE, interpolation=cv2.INTER_NEAREST, p=0.5),
-            # A.CoarseDropout(max_holes=4,max_height=32,max_width=32,p=0.5),
-            # A.RandomCrop(height=160,width=160,p=0.5),
-            A.Affine(translate_percent=[0.05, 0.1], mode=cv2.BORDER_CONSTANT, interpolation=cv2.INTER_NEAREST, p=0.6),
-            # A.CoarseDropout(max_holes=4,max_height=32,max_width=32,p=0.5),
-        ], keypoint_params=A.KeypointParams(format='xy'))
+        self.data_root = check_file(data_root)
 
-        self.point_num = 1
+        self.test = test_mode
+        self.pipeline = Pose_Compose(pipeline,
+                                     keypoint_params=A.KeypointParams(format))
+        self.test_trans = transforms.Compose([transforms.ToTensor()])
 
-        with open(index_file, 'r') as f:
+        with open(os.path.join(self.data_root, index_file), 'r') as f:
             self.lines = f.readlines()
         self.flag = np.zeros(self.__len__(), dtype=np.uint8)
 
     def __getitem__(self, item):
         self.line = self.lines[item].strip().split()
-        img_file = './datasets/' + self.line[0].replace('\\', '/')          #todo
+        img_file = os.path.join(self.data_root, self.line[0])
         img_file = str(img_file)
         self.img = cv2.imread(img_file)
         self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
-        self.landmark = np.asarray(self.line[1:5], dtype=np.float32)
-
-        self.landmark = np.asarray(self.line[1:], dtype=np.float32)
-        point = []
-        point.append(self.landmark[0])
-        point.append(self.landmark[1])
+        w = self.img.shape[1]
+        points = np.asarray(self.line[1:], dtype=np.float32)
+        point_num = len(points) // 2
         landmark = []
-        for i in range(self.point_num):
-            landmark.append([point[i * 2], point[i * 2 + 1]])
+        for i in range(point_num):
+            landmark.append([points[i * 2], points[i * 2 + 1]])
 
-        if self.train:
+        if self.test:
+            img, label = self.test_trans(
+                self.img), np.asarray(landmark).flatten() / w
+        else:
             while True:
-                result = self.transforms(image=self.img, keypoints=landmark)
-                if len(result['keypoints']) < self.point_num:
+                result = self.pipeline(image=self.img, keypoints=landmark)
+                if len(result['keypoints']) < point_num:
                     continue
                 else:
                     break
-            img, label = self.test_trans(result['image']), np.asarray(result['keypoints']).flatten() / 240
-        else:
-            img, label = self.test_trans(self.img), np.asarray(landmark).flatten() / 240
+            img, label = self.test_trans(
+                result['image']), np.asarray(result['keypoints']).flatten() / w
 
         return {'img': img, 'img_metas': label}
 
@@ -96,4 +86,8 @@ class MeterData(Dataset):
         return len(self.lines)
 
     def evaluate(self, results, **kwargs):
-        return {'loss': torch.mean(torch.tensor([i['loss'] for i in results])).cpu().item()}
+        return {
+            'loss':
+            torch.mean(torch.tensor([i['loss']
+                                     for i in results])).cpu().item()
+        }
