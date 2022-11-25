c0e0975fe5195c13ff1bedbb82302af69f943d1b
mjq2020 <mjqx2011@163.com>
2022-11-25 12:13:44 +0000

    fix data_root is None bug

--- datasets/cocodataset.py
+++ datasets/cocodataset.py
@@ -19,10 +19,12 @@ class CustomCocoDataset(CocoDataset):
                  test_mode=False,
                  filter_empty_gt=True,
                  file_client_args=dict(backend='disk')):
-
-        data_root = eval(data_root) if (data_root is None or '[' in data_root )else data_root
-        data_root = check_file(data_root,
-                               data_name="coco") if data_root else data_root
+        if data_root is None:
+            pass
+        else:
+            data_root = eval(data_root) if '[' in data_root else data_root
+            data_root = check_file(
+                data_root, data_name="coco") if data_root else data_root
         super().__init__(ann_file, pipeline, classes, data_root, img_prefix,
                          seg_prefix, seg_suffix, proposal_file, test_mode,
                          filter_empty_gt, file_client_args)
