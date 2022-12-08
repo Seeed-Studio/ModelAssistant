2c8c2b18bc410e3bb60ce495bfc1f13d923b47e5
mjq2020 <mjqx2011@163.com>
2022-12-08 01:15:37 +0000

    add import init

--- core/hook/__init__.py
+++ core/hook/__init__.py
@@ -1,3 +1,5 @@
 from .audio_hooks import Audio_hooks
+from .train_log import ProgressLog
+from .logger import TextLoggerHook
 
-__all__ = ['Audio_hooks']
+__all__ = ['Audio_hooks', 'ProgressLog','TextLoggerHook']
