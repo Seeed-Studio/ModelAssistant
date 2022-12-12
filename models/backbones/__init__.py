8b38c8f8fa4cb840dc6907522838c962d63449a9
mjq2020 <mjqx2011@163.com>
2022-12-12 07:43:42 +0000

    change init import

--- models/backbones/__init__.py
+++ models/backbones/__init__.py
@@ -1,4 +1,4 @@
-from .pfld import PFLDInference
+from .pfld_mobilenet_v2 import PfldMobileNetV2
 from .SoundNet import SoundNetRaw
 
-__all__ = ['SoundNetRaw']
+__all__ = ['PfldMobileNetV2','SoundNetRaw']
