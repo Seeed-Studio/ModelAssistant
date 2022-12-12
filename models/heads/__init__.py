db6d7423266b1e47c9bdd4006837e7bc3fef08b6
mjq2020 <mjqx2011@163.com>
2022-12-12 02:48:42 +0000

    add pfld model head

--- models/heads/__init__.py
+++ models/heads/__init__.py
@@ -1,4 +1,5 @@
 from .cls_head import Audio_head
 from .taggregate_head import TAggregate
+from .pfld_head import PFLDhead
 
-__all__ = ['Audio_head', 'TAggregate']
+__all__ = ['Audio_head', 'TAggregate','PFLDhead']
