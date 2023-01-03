ff6b0144b02cad0b53c286c347e88bc84be94ddd
mjq2020 <mjqx2011@163.com>
2023-01-03 09:40:40 +0000

    remove fomoloss

--- models/losses/__init__.py
+++ models/losses/__init__.py
@@ -2,9 +2,7 @@ from .classfication_loss import LabelSmoothCrossEntropyLoss
 from .pfld_loss import PFLDLoss
 from .nll_loss import NLLLoss
 from .bce_withlogits_loss import BCEWithLogitsLoss
-from .fomo_loss import FomoLoss
 
 __all__ = [
-    'LabelSmoothCrossEntropyLoss', 'PFLDLoss', 'NLLLoss',
-    'BCEWithLogitsLoss', 'FomoLoss'
+    'LabelSmoothCrossEntropyLoss', 'PFLDLoss', 'NLLLoss', 'BCEWithLogitsLoss'
 ]
