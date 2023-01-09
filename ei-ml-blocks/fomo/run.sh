735b4cc62fd801610330c68bc60895ede42e1c06
LynnL4 <lht856@foxmail.com>
2023-01-09 14:47:42 +0800

    fix: fomo ei ml block.

--- ei-ml-blocks/fomo/run.sh
+++ ei-ml-blocks/fomo/run.sh
@@ -71,17 +71,17 @@ cd /app/Edgelab
 # add the current directory to the PYTHONPATH
 export PYTHONPATH=/app/Edgelab:$PYTHONPATH
 
-python3 -u tools/train.py mmdet configs/fomo/fomo_mobnetv2_x8_coco.py \
+#copy the config file to the config directory
+cp $SCRIPTPATH/fomo_mobnetv2_x8_ei.py /app/Edgelab/configs/fomo/fomo_mobnetv2_x8_ei.py
+
+python3 -u tools/train.py mmdet configs/fomo/fomo_mobnetv2_x8_ei.py \
     --cfg-options \
     runner.max_epochs=$EPOCHS   \
+    model.head.num_classes=$NUM_CLASS \
     optimizer.lr=$LEARNING_RATE \
-    data.train.dataset.pipeline.4.img_scale=\($IMG_HIGHT,$IMG_WIDTH\) \
+    data.train.dataset.pipeline.2.img_scale=\($IMG_HIGHT,$IMG_WIDTH\) \
     data.val.pipeline.1.img_scale=\($IMG_HIGHT,$IMG_WIDTH\) \
-    data.test.pipeline.1.img_scale=\($IMG_HIGHT,$IMG_WIDTH\) \
-    data.train.dataset.data_root=/tmp/data \
-    data.val.data_root=/tmp/data \
-    data.test.data_root=/tmp/data 
-
+    data.test.pipeline.1.img_scale=\($IMG_HIGHT,$IMG_WIDTH\) 
 
 echo "Training complete"
 echo ""
@@ -89,16 +89,16 @@ echo ""
 mkdir -p $OUT_DIRECTORY
 
 #copy the model to the output directory
-cp /app/Edgelab/work_dirs/fomo_mobnetv2_x8_coco/exp1/latest.pth $OUT_DIRECTORY/model.pth
+cp /app/Edgelab/work_dirs/fomo_mobnetv2_x8_ei/exp1/latest.pth $OUT_DIRECTORY/model.pth
 
 # convert to .onnx
-# python3 tools/torch2onnx.py --task mmdet --shape $IMG_HIGHT --config configs/fomo/fomo_mobnetv2_x8_coco.py --checkpoint work_dirs/fomo_mobnetv2_x8_coco/exp1/latest.pth 
-# cp /app/Edgelab/work_dirs/fomo_mobnetv2_x8_coco/exp1/latest.onnx $OUT_DIRECTORY/model.onnx
+# python3 tools/torch2onnx.py --task mmdet --shape $IMG_HIGHT --config configs/fomo/fomo_mobnetv2_x8_ei.py --checkpoint work_dirs/fomo_mobnetv2_x8_ei/exp1/latest.pth 
+# cp /app/Edgelab/work_dirs/fomo_mobnetv2_x8_ei/exp1/latest.onnx $OUT_DIRECTORY/model.onnx
 
-python3 ./tools/export.py mmdet /app/Edgelab/work_dirs/fomo_mobnetv2_x8_coco/exp1/fomo_mobnetv2_x8_coco.py  --tflite_type fp32 --weights $OUT_DIRECTORY/model.pth --shape $IMG_HIGHT --data /tmp/data/train
+python3 ./tools/export.py mmdet /app/Edgelab/work_dirs/fomo_mobnetv2_x8_ei/exp1/fomo_mobnetv2_x8_ei.py  --tflite_type fp32 --weights $OUT_DIRECTORY/model.pth --shape $IMG_HIGHT --data /tmp/data/train
 
 mv $OUT_DIRECTORY/model_fp32.tflite $OUT_DIRECTORY/model.tflite
 
-python3 ./tools/export.py mmdet /app/Edgelab/work_dirs/fomo_mobnetv2_x8_coco/exp1/fomo_mobnetv2_x8_coco.py  --tflite_type int8 --weights $OUT_DIRECTORY/model.pth --shape $IMG_HIGHT --data /tmp/data/train
+python3 ./tools/export.py mmdet /app/Edgelab/work_dirs/fomo_mobnetv2_x8_ei/exp1/fomo_mobnetv2_x8_ei.py  --tflite_type int8 --weights $OUT_DIRECTORY/model.pth --shape $IMG_HIGHT --data /tmp/data/train
 
 mv $OUT_DIRECTORY/model_int8.tflite $OUT_DIRECTORY/model_quantized_int8_io.tflite
