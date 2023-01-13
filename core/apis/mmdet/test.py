855b7fddeacf3037362b2b13413973e38e72d6fd
jqm <jinqing.mu@seeed.cc>
2023-01-13 15:38:13 +0800

    change result apend

--- core/apis/mmdet/test.py
+++ core/apis/mmdet/test.py
@@ -7,13 +7,13 @@ def sigle_gpu_test_fomo(model, dataloader):
     datasets = dataloader.dataset
     prog_bar = mmcv.ProgressBar(len(datasets))
 
-    results = []
+    preds = []
+    targets = []
     for idx, data in enumerate(dataloader):
         with torch.no_grad():
-            result = model(return_loss=False, fomo=True, **data)
-            results.append(result)
+            pred, target = model(return_loss=False, fomo=True, **data)
+            preds.append(pred)
+            targets.append(target)
 
-        batch_size = len(result)
-        for _ in range(batch_size):
-            prog_bar.update()
-    return results
+        prog_bar.update()
+    return dict(preds=preds, targets=targets)
