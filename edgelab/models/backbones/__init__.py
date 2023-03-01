6a7b2f2e8453f8e4138ca7367ed5cb4bb781f148
LynnL4 <lht856@foxmail.com>
2023-03-01 01:58:36 +0000

    backones: add AxesNet for accelerometer classify.

--- edgelab/models/backbones/__init__.py
+++ edgelab/models/backbones/__init__.py
@@ -1,5 +1,6 @@
 from .pfld_mobilenet_v2 import PfldMobileNetV2
 from .SoundNet import SoundNetRaw
 from .shufflenetv2 import CustomShuffleNetV2
+from .AxesNet import AxesNet
 
-__all__ = ['PfldMobileNetV2', 'SoundNetRaw', 'CustomShuffleNetV2']
+__all__ = ['PfldMobileNetV2', 'SoundNetRaw', 'CustomShuffleNetV2', 'AxesNet']
