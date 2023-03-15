be947c64ed4ddd7bf09ffa3011da32c78b1e4090
mjq2020 <mjqx2011@163.com>
2023-03-15 12:52:41 +0000

    add base module config

--- edgelab/models/base/general.py
+++ edgelab/models/base/general.py
@@ -1,5 +1,83 @@
+from typing import Optional,Callable,Dict,AnyStr
+import types
 import torch.nn as nn
+from mmcv.cnn.bricks.norm import NORM_LAYERS
+from mmcv.cnn.bricks.activation import ACTIVATION_LAYERS
+from mmcv.cnn.bricks.conv import CONV_LAYERS
 
+
+def get_conv(conv):
+    if isinstance(conv,dict) and hasattr(nn,conv.get('type')):
+            conv=getattr(nn,conv.get('type'))
+    elif isinstance(conv,str) and hasattr(nn,conv):
+        conv=getattr(nn,conv)
+    elif isinstance(conv,str) and conv in CONV_LAYERS.module_dict:
+        conv=CONV_LAYERS.get(conv)
+    elif isinstance(conv,type.__class__) and issubclass(conv,nn.Module):
+        pass
+    else:
+        raise ValueError('Unable to parse the value of conv_layer, please confirm whether the value of conv_layer is correct')
+    return conv
+
+def get_norm(norm):
+    if isinstance(norm,dict) and hasattr(nn,norm['type']):
+            norm=getattr(nn,norm.get('type'))
+    elif isinstance(norm,dict) and norm.get('type') in NORM_LAYERS.module_dict:
+        norm=NORM_LAYERS.get(norm.get('type'))
+    elif isinstance(norm,str) and hasattr(nn,norm):
+        norm=getattr(nn,norm)
+    elif isinstance(norm,str) and norm in NORM_LAYERS.module_dict:
+        norm=NORM_LAYERS.get(norm)
+    elif isinstance(norm,type.__class__) and issubclass(norm,nn.Module):
+        pass
+    else:
+        raise ValueError('Unable to parse the value of norm_layer, please confirm whether the value of norm_layer is correct')
+    return norm
+
+def get_act(act):
+    if isinstance(act,dict) and hasattr(nn,act.get('type')):
+        act=getattr(nn,act.get('type'))
+    elif isinstance(act,str) and hasattr(nn,act):
+        act=getattr(nn,act)
+    elif isinstance(act,str) and act in ACTIVATION_LAYERS.module_dict:
+        act=ACTIVATION_LAYERS.get(act)
+    elif isinstance(act,type.__class__) and issubclass(act,nn.Module):
+        pass
+    else:
+        raise ValueError('Unable to parse the value of act_layer, please confirm whether the value of act_layer is correct')
+    return act
+
+class ConvNormActivation(nn.Sequential):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        conv_layer:Optional[Callable[..., nn.Module]] or Dict or AnyStr=None,
+        kernel_size: int = 3,
+        stride: int = 1,
+        padding: Optional[int] = None,
+        groups: int = 1,
+        norm_layer: Optional[Callable[..., nn.Module]] or Dict or AnyStr = nn.BatchNorm2d,
+        activation_layer: Optional[Callable[..., nn.Module]] or Dict or AnyStr = nn.ReLU,
+        dilation: int = 1,
+        inplace: bool = True,
+    ) -> None:
+        if padding is None:
+            padding = (kernel_size - 1) // 2 * dilation
+        if conv_layer is None:
+            conv_layer=nn.Conv2d
+        else:
+            conv_layer=get_conv(conv_layer)
+        layers = [conv_layer(in_channels, out_channels, kernel_size, stride, padding,
+                                  dilation=dilation, groups=groups, bias=norm_layer is None)]
+        if norm_layer is not None:
+            norm_layer=get_norm(norm_layer)
+            layers.append(norm_layer(out_channels))
+        if activation_layer is not None:
+            activation_layer=get_act(activation_layer)
+            layers.append(activation_layer(inplace=inplace))
+        super().__init__(*layers)
+        self.out_channels = out_channels
 
 def CBR(inp, oup, kernel, stride, bias=False, padding=1, groups=1, act='ReLU'):
     return nn.Sequential(
