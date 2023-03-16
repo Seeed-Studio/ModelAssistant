6b2c443549cb12e957f3e222f6a14d3dcecba174
mjq2020 <mjqx2011@163.com>
2023-03-16 13:38:07 +0000

    add backbone import

--- edgelab/models/backbones/__init__.py
+++ edgelab/models/backbones/__init__.py
@@ -3,5 +3,10 @@ from .SoundNet import SoundNetRaw
 from .shufflenetv2 import CustomShuffleNetV2
 from .AxesNet import AxesNet
 from .MobileNetv3 import MobileNetV3
+from .ShuffleNetV2 import ShuffleNetV2
+from .SqueezeNet import SqueezeNet
 
-__all__ = ['PfldMobileNetV2', 'SoundNetRaw', 'CustomShuffleNetV2', 'AxesNet','MobileNetV3']
+__all__ = [
+    'PfldMobileNetV2', 'SoundNetRaw', 'CustomShuffleNetV2', 'AxesNet',
+    'MobileNetV3', 'ShuffleNetV2', 'SqueezeNet'
+]
