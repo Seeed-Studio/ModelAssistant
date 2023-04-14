749d009774d439a544e273a227871fd857850394
mjq2020 <mjqx2011@163.com>
2023-04-14 11:07:06 +0000

    mmlab2.0 config

--- configs/pfld/pfld_mv2n_112.py
+++ configs/pfld/pfld_mv2n_112.py
@@ -1,4 +1,4 @@
-_base_ = '../_base_/default_runtime.py'
+_base_ = '../_base_/default_runtime_pose.py'
 
 num_classes=1
 model = dict(type='PFLD',
@@ -18,7 +18,7 @@ model = dict(type='PFLD',
 # dataset settings
 dataset_type = 'MeterData'
 
-data_root = '~/datasets/meter'
+data_root = '/home/dq/datasets/meter/'
 height=112
 width=112
 batch_size=32
@@ -39,33 +39,40 @@ val_pipeline = [dict(type="Resize", height=height, width=width)]
 
 
 
-data = dict(
-    samples_per_gpu=batch_size,
-    workers_per_gpu=workers,
-    train=dict(type=dataset_type,
+train_dataloader = dict(
+    batch_size=32,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False, round_up=False),
+    dataset=dict(type=dataset_type,
                data_root=data_root,
                index_file=r'train/annotations.txt',
                pipeline=train_pipeline,
-               test_mode=False),
-    val=dict(type=dataset_type,
+               test_mode=False),)
+
+val_dataloader=dict(
+    batch_size=32,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False, round_up=False),
+    dataset=dict(type=dataset_type,
              data_root=data_root,
              index_file=r'val/annotations.txt',
              pipeline=val_pipeline,
-             test_mode=True),
-    test=dict(type=dataset_type,
-              data_root=data_root,
-              index_file=r'val/annotations.txt',
-              pipeline=val_pipeline,
-              test_mode=True
-              # dataset_info={{_base_.dataset_info}}
-              ))
+             test_mode=True),)
+test_dataloader=val_dataloader
 
 
 lr=0.0001
 epochs=300
 evaluation = dict(save_best='loss')
-optimizer = dict(type='Adam', lr=lr, betas=(0.9, 0.99), weight_decay=1e-6)
+optim_wrapper=dict(optimizer = dict(type='Adam', lr=lr, betas=(0.9, 0.99), weight_decay=1e-6))
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+val_evaluator=dict(
+    type='CocoMetric')
+test_evaluator = val_evaluator
 # learning policy
 lr_config = dict(policy='step',
                  warmup='linear',
