27d921fd8860747a6976eb8c55c90cfd6a6eaee1
mjq2020 <mjqx2011@163.com>
2023-04-19 07:03:17 +0000

    pfld lab2.0 config

--- configs/pfld/pfld_mv2n_112.py
+++ configs/pfld/pfld_mv2n_112.py
@@ -1,28 +1,25 @@
 _base_ = '../_base_/default_runtime_pose.py'
 
-num_classes=1
+num_classes = 1
 model = dict(type='PFLD',
              backbone=dict(type='PfldMobileNetV2',
                            inchannel=3,
                            layer1=[16, 16, 16, 16, 16],
                            layer2=[32, 32, 32, 32, 32, 32],
                            out_channel=16),
-             head=dict(
-                 type='PFLDhead',
-                 num_point=num_classes,
-                 input_channel=16,
-             ),
-             loss_cfg=dict(type='PFLDLoss'))
-
+             head=dict(type='PFLDhead',
+                       num_point=num_classes,
+                       input_channel=16,
+                       loss_cfg=dict(type='L1Loss')))
 
 # dataset settings
 dataset_type = 'MeterData'
 
-data_root = '/home/dq/datasets/meter/'
-height=112
-width=112
-batch_size=32
-workers=4
+data_root = ''
+height = 112
+width = 112
+batch_size = 32
+workers = 4
 
 train_pipeline = [
     dict(type="Resize", height=height, width=width, interpolation=0),
@@ -37,44 +34,55 @@ train_pipeline = [
 
 val_pipeline = [dict(type="Resize", height=height, width=width)]
 
-
-
 train_dataloader = dict(
     batch_size=32,
     num_workers=2,
     persistent_workers=True,
     drop_last=False,
     collate_fn=dict(type='default_collate'),
-    sampler=dict(type='DefaultSampler', shuffle=False, round_up=False),
+    sampler=dict(type='DefaultSampler', shuffle=True, round_up=False),
     dataset=dict(type=dataset_type,
-               data_root=data_root,
-               index_file=r'train/annotations.txt',
-               pipeline=train_pipeline,
-               test_mode=False),)
+                 data_root=data_root,
+                 index_file=r'train/annotations.txt',
+                 pipeline=train_pipeline,
+                 test_mode=False),
+)
 
-val_dataloader=dict(
-    batch_size=32,
-    num_workers=2,
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=1,
     persistent_workers=True,
     drop_last=False,
     collate_fn=dict(type='default_collate'),
     sampler=dict(type='DefaultSampler', shuffle=False, round_up=False),
     dataset=dict(type=dataset_type,
-             data_root=data_root,
-             index_file=r'val/annotations.txt',
-             pipeline=val_pipeline,
-             test_mode=True),)
-test_dataloader=val_dataloader
+                 data_root=data_root,
+                 index_file=r'val/annotations.txt',
+                 pipeline=val_pipeline,
+                 test_mode=True),
+)
+test_dataloader = val_dataloader
 
-
-lr=0.0001
-epochs=300
+lr = 0.0001
+epochs = 300
 evaluation = dict(save_best='loss')
-optim_wrapper=dict(optimizer = dict(type='Adam', lr=lr, betas=(0.9, 0.99), weight_decay=1e-6))
+optim_wrapper = dict(
+    optimizer=dict(type='Adam', lr=lr, betas=(0.9, 0.99), weight_decay=1e-6))
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
-val_evaluator=dict(
-    type='PointMetric')
+val_evaluator = dict(type='PointMetric')
 test_evaluator = val_evaluator
 
-total_epochs = epochs
 find_unused_parameters = True
+
+train_cfg = dict(by_epoch=True, max_epochs=500)
+# learning policy
+param_scheduler = [
+    dict(type='LinearLR', begin=0, end=500, start_factor=0.001,
+         by_epoch=False),  # warm-up
+    dict(type='MultiStepLR',
+         begin=1,
+         end=500,
+         milestones=[350, 400, 450, 490],
+         gamma=0.1,
+         by_epoch=True)
+]
