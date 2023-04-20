2c5833f22e82ec57f1595b3a3a220e1b0c70cb1a
mjq2020 <mjqx2011@163.com>
2023-04-20 01:57:07 +0000

    remove audio hook import

--- edgelab/core/__init__.py
+++ edgelab/core/__init__.py
@@ -1,6 +1,6 @@
-from .hook import Audio_hooks, TextLoggerHook, TensorboardLoggerHook, WandbLoggerHook
+from .hook import TextLoggerHook, TensorboardLoggerHook, WandbLoggerHook
 
 __all__ = [
-    'Audio_hooks', 'TextLoggerHook', 'TensorboardLoggerHook',
+    'TextLoggerHook', 'TensorboardLoggerHook',
     'WandbLoggerHook', 'PaviLoggerHook', 'ClearMLLoggerHook'
 ]
--- edgelab/core/hook/__init__.py
+++ edgelab/core/hook/__init__.py
@@ -1,7 +1,6 @@
-from .audio_hooks import Audio_hooks
 from .logger import TextLoggerHook, TensorboardLoggerHook, WandbLoggerHook, ClearMLLoggerHook
 
 __all__ = [
-    'Audio_hooks', 'TextLoggerHook', 'TensorboardLoggerHook',
+    'TextLoggerHook', 'TensorboardLoggerHook',
     'WandbLoggerHook', 'PaviLoggerHook', 'ClearMLLoggerHook'
 ]
