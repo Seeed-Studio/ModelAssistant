b934ee5f7d03a46b6b0dfac2e73fea651ab118f9
mjq2020 <mjqx2011@163.com>
2023-04-20 01:52:28 +0000

    audio classify lab2.0 config

--- configs/_base_/default_runtime_cls.py
+++ configs/_base_/default_runtime_cls.py
@@ -36,7 +36,7 @@ env_cfg = dict(
 
 # set visualizer
 vis_backends = [dict(type='LocalVisBackend')]
-visualizer = dict(type='mmcls.UniversalVisualizer', vis_backends=vis_backends)
+# visualizer = dict(type='mmcls.UniversalVisualizer', vis_backends=vis_backends)
 
 # set log level
 log_level = 'INFO'
--- configs/audio_classify/ali_classiyf_small_8k_8192.py
+++ configs/audio_classify/ali_classiyf_small_8k_8192.py
@@ -1,4 +1,4 @@
-_base_ = ['../_base_/default_runtime.py']
+_base_ = ['../_base_/default_runtime_cls.py']
 
 words = [
     "no",
@@ -13,8 +13,13 @@ words = [
     'stop', 'three', 'tree', 'two', 'up', 'visual', 'wow', 'yes', 'zero'
 ]
 # model settings
-num_classes=35
+num_classes = 35
 model = dict(type='Audio_classify',
+             n_cls=len(words),
+                multilabel=False,
+            loss=dict(type='LabelSmoothCrossEntropyLoss',
+                   reduction='sum',
+                   smoothing=0.1),
              backbone=dict(type='SoundNetRaw',
                            nf=2,
                            clip_length=64,
@@ -37,96 +42,83 @@ transforms = [
 ]
 
 data_root = 'http://download.tensorflow.org/data/speech_commands_v0.02.tar.gz'
-width=8192
-batch_size=128
-workers=8
+width = 8192
+batch_size = 128
+workers = 8
 
 train_pipeline = dict(type='AudioAugs', k_augs=transforms)
 
-img_norm_cfg = dict(mean=[123.675, 116.28, 103.53],
-                    std=[58.395, 57.12, 57.375],
-                    to_rgb=True)
-test_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='CenterCrop',
-         crop_size=224,
-         efficientnet_style=True,
-         interpolation='bicubic',
-         backend='pillow'),
-    dict(type='Normalize',
-         mean=[123.675, 116.28, 103.53],
-         std=[58.395, 57.12, 57.375],
-         to_rgb=True),
-    dict(type='ImageToTensor', keys=['img']),
-    dict(type='Collect', keys=['img'])
-]
 
-data = dict(
-    samples_per_gpu=batch_size,
-    workers_per_gpu=workers,
-    train=dict(type=dataset_type,
-               root=data_root,
-               sampling_rate=8000,
-               segment_length=width,
-               pipeline=train_pipeline,
-               mode='train',
-               use_background=True,
-               lower_volume=True,
-               words=words),
-    val=dict(type=dataset_type,
-             root=data_root,
-             sampling_rate=8000,
-             segment_length=width,
-             mode='val',
-             use_background=False,
-             lower_volume=True,
-             words=words),
-    test=dict(
-        type=dataset_type,
-        root=data_root,
-        sampling_rate=8000,
-        segment_length=width,
-        mode='test',
-        use_background=False,
-        lower_volume=True,
-        words=words))
+train_dataloader = dict(
+    batch_size=64,
+    num_workers=8,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=dict(type=dataset_type,
+                 root=data_root,
+                 sampling_rate=8000,
+                 segment_length=width,
+                 pipeline=train_pipeline,
+                 mode='train',
+                 use_background=True,
+                 lower_volume=True,
+                 words=words),
+)
+val_dataloader = dict(
+    batch_size=32,
+    num_workers=4,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=dict(type=dataset_type,
+                 root=data_root,
+                 sampling_rate=8000,
+                 segment_length=width,
+                 mode='val',
+                 use_background=False,
+                 lower_volume=True,
+                 words=words),
+)
+test_dataloader = val_dataloader
 
-custom_hooks = dict(type='Audio_hooks',
-                    n_cls=len(words),
-                    multilabel=False,
-                    loss=dict(type='LabelSmoothCrossEntropyLoss',
-                              reduction='sum',
-                              smoothing=0.1),
-                    seq_len=width,
-                    sampling_rate=8000,
-                    device='0',
-                    augs_mix=['mixup', 'timemix', 'freqmix', 'phmix'],
-                    mix_ratio=1,
-                    local_rank=0,
-                    epoch_mix=12,
-                    mix_loss='bce',
-                    priority=0)
 
-evaluation = dict(save_best='acc',
-                  interval=1,
-                  metric='accuracy',
-                  metric_options={'topk': (1, )})
+data_preprocessor = dict(type='ETADataPreprocessor',
+                         n_cls=len(words),
+                         multilabel=False,
+                         seq_len=width,
+                         sampling_rate=8000,
+
+                         augs_mix=['mixup', 'timemix', 'freqmix', 'phmix'],
+                         mix_ratio=1,
+                         local_rank=0,
+                         epoch_mix=12,
+                         mix_loss='bce')
 
 # optimizer
-lr=0.0003
-epochs=1500
-optimizer = dict(type='AdamW',
-                 lr=lr,
-                 betas=[0.9, 0.99],
-                 weight_decay=0,
-                 eps=1e-8)
-optimizer_config = dict(grad_clip=None)
+lr = 0.0003
+epochs = 1500
+find_unused_parameters = True
+
+optim_wrapper = dict(optimizer=dict(
+    type='AdamW', lr=lr, betas=(0.9, 0.99), weight_decay=5e-4, eps=1e-7))
+optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+
+#evaluator
+val_evaluator = dict(type='mmcls.Accuracy',topk=(1, 5,))
+test_evaluator = val_evaluator
+
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=1000)
+val_cfg = dict()
+test_cfg = dict()
 # learning policy
-# lr_config = dict(policy='OneCycleLR', max_lr=3e-4, steps_per_epoch=200, epochs=1000, pct_start=0.1, )
-# lr_config = dict(policy='step', step=[50,200])
-lr_config = dict(
-    policy='OneCycle',
-    max_lr=lr,
-    # steps_per_epoch=388,
-    # epoch=1500,
-    pct_start=0.1)
+param_scheduler = [
+    dict(type='LinearLR', begin=0, end=30, start_factor=0.001,
+         by_epoch=False),  # warm-up
+    dict(type='MultiStepLR',
+         begin=1,
+         end=500,
+         milestones=[100, 200, 250],
+         gamma=0.1,
+         by_epoch=True)
+]
