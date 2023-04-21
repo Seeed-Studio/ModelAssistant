2c27b1c679f7a4afae4d16a2a03c7066abaf8d33
mjq2020 <mjqx2011@163.com>
2023-04-21 06:20:11 +0000

    add fpn with fomo

---
+++ configs/fomo/fomo_mobnetv2_fpn_0.35_x8_abl.py
@@ -0,0 +1,26 @@
+_base_ = './fomo_mobnetv2_0.35_x8_abl_coco.py'
+
+num_classes = 2
+model = dict(
+    backbone=dict(type='mmdet.MobileNetV2',
+                  widen_factor=0.35,
+                  out_indices=(2, 3, 5)),
+    neck=dict(
+        type='mmdet.FPN',
+        in_channels=[16, 24, 56],
+        out_channels=24,
+        num_outs=3,
+    ),
+    head=dict(
+        type='FomoHead',
+        input_channels=24,
+        num_classes=num_classes,
+        middle_channels=[96, 32],
+        act_cfg='ReLU6',
+        loss_cls=dict(type='BCEWithLogitsLoss',
+                      reduction='none',
+                      pos_weight=40),
+        loss_bg=dict(type='BCEWithLogitsLoss', reduction='none'),
+        cls_weight=40,
+    ),
+)
