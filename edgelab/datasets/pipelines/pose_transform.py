0508edfd7fd634baf435c51d9181e8e7c5902519
mjq2020 <mjqx2011@163.com>
2023-04-28 08:26:46 +0000

    change class name

--- edgelab/datasets/pipelines/pose_transform.py
+++ edgelab/datasets/pipelines/pose_transform.py
@@ -1,22 +1,42 @@
+from typing import Optional, Union, Dict, Sequence
+
 import albumentations as A
+from albumentations.core.bbox_utils import BboxParams
+from albumentations.core.keypoints_utils import KeypointParams
+from albumentations.core.composition import BaseCompose, BasicTransform
 
 from edgelab.registry import TRANSFORMS
 
 
-class Pose_Compose(A.Compose):
+class AlbCompose(A.Compose):
+    """
+    The packaging of the compose class of alb, the purpose 
+    is to parse the pipeline in the configuration file
+    
+    Args:
+        tranforms(list):The packaging of the compose class of alb, the purpose is to 
+            parse the pipeline in the configuration file
+        bbox_params (BboxParams): Parameters for bounding boxes transforms
+        keypoint_params (KeypointParams): Parameters for keypoints transforms
+        additional_targets (dict): Dict with keys - new target name, values - old 
+            target name. ex: {'image2': 'image'}
+        p (float): probability of applying all list of transforms. Default: 1.0.
+    
+    """
 
     def __init__(self,
-                 transforms,
-                 bbox_params=None,
-                 keypoint_params=None,
-                 additional_targets=None,
+                 transforms: Sequence[Dict],
+                 bbox_params: Optional[Union[dict, "BboxParams"]] = None,
+                 keypoint_params: Optional[Union[dict,
+                                                 "KeypointParams"]] = None,
+                 additional_targets: Optional[Dict[str, str]] = None,
                  p: float = 1):
         pose_trans = []
         for transform in transforms:
             if isinstance(transform, dict):
                 transform = TRANSFORMS.build(transform)
                 pose_trans.append(transform)
-            elif callable(transform):
+            elif isinstance(transforms, (BaseCompose, BasicTransform)):
                 pose_trans.append(transform)
             else:
                 raise TypeError('transform must be callable or a dict, but got'
