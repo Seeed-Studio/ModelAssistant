7a403522796d8e1f2deb007978a870ee1cae20c9
mjq2020 <mjqx2011@163.com>
2023-05-04 06:38:25 +0000

    add image index

--- edgelab/datasets/utils/functions.py
+++ edgelab/datasets/utils/functions.py
@@ -5,8 +5,8 @@ from mmdet.structures import DetDataSample
 @FUNCTIONS.register_module()
 def fomo_collate(batch):
     img, label = [x['inputs'] for x in batch], [y['data_samples'] for y in batch]
-    # for i, l in enumerate(label):
-    #     if l.shape[0] > 0:
-    #         l[:, 0] = i
+    for i, l in enumerate(label):
+        if l.shape[0] > 0:
+            l[:, 0] = i
     return dict(inputs=torch.stack(img), data_samples=[DetDataSample(labels=torch.cat(label, 0))])
  
