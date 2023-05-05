5f42e8e98e6b216d16fb1b87097205d0df863f4f
mjq2020 <mjqx2011@163.com>
2023-05-05 08:00:39 +0000

    Add Registrar

--- edgelab/registry.py
+++ edgelab/registry.py
@@ -7,6 +7,14 @@ from mmengine.registry import METRICS as MMENGINE_METRICS
 from mmengine.registry import TRANSFORMS as MMENGINE_TRANSFORMS
 from mmengine.registry import FUNCTIONS as MMENGINE_FUNCTIONS
 from mmengine.registry import LOOPS as MMENGINE_LOOPS
+from mmengine.registry import OPTIMIZERS as MMENGINE_OPTIMIZERS
+from mmengine.registry import OPTIM_WRAPPERS as MMENGINE_OPTIM_WRAPPERS
+from mmengine.registry import OPTIM_WRAPPER_CONSTRUCTORS as MMENGINE_OPTIM_WRAPPER_CONSTRUCTORS
+from mmengine.registry import PARAM_SCHEDULERS as MMENGINE_PARAM_SCHEDULERS
+from mmengine.registry import VISBACKENDS as MMENGINE_VISBACKENDS
+from mmengine.registry import VISUALIZERS as MMENGINE_VISUALIZERS
+from mmengine.registry import LOG_PROCESSORS as MMENGINE_LOG_PROCESSORS
+
 from mmengine.registry import Registry
 
 BACKBONES = MMENGINE_MODELS
@@ -14,6 +22,33 @@ NECKS = MMENGINE_MODELS
 HEADS = MMENGINE_MODELS
 LOSSES = MMENGINE_MODELS
 POSE_ESTIMATORS = MMENGINE_MODELS
+
+LOG_PROCESSORS = Registry('log processors',
+                          parent=MMENGINE_LOG_PROCESSORS,
+                          locations=['edgelab'])
+
+VISBACKENDS = Registry('visbackends',
+                       parent=MMENGINE_VISBACKENDS,
+                       locations=['edgelab'])
+VISUALIZERS = Registry('visualizers',
+                       parent=MMENGINE_VISUALIZERS,
+                       locations=['edgelab'])
+
+OPTIM_WRAPPERS = Registry('optim_wrapper',
+                          parent=MMENGINE_OPTIM_WRAPPERS,
+                          locations=['edgelab'])
+
+OPTIMIZERS = Registry('optimizer',
+                      parent=MMENGINE_OPTIMIZERS,
+                      locations=['edgelab'])
+OPTIM_WRAPPER_CONSTRUCTORS = Registry(
+    'optimizer wrapper constructor',
+    parent=MMENGINE_OPTIM_WRAPPER_CONSTRUCTORS,
+    locations=['edgelab'])
+
+PARAM_SCHEDULERS = Registry('param schedulers',
+                            parent=MMENGINE_PARAM_SCHEDULERS,
+                            locations=['edgelab'])
 
 LOOPS = Registry('loop',
                  parent=MMENGINE_LOOPS,
