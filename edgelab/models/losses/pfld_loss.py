98814c949a6d8a8644e2245443ad48dd566d22e3
mjq2020 <mjqx2011@163.com>
2023-05-09 11:49:32 +0000

    change loss to sqrt

--- edgelab/models/losses/pfld_loss.py
+++ edgelab/models/losses/pfld_loss.py
@@ -15,7 +15,6 @@ class PFLDLoss(nn.Module):
 
     def forward(self, landmarks, landmark_gt):
         # angle_loss = torch.sum(1-torch.cos((angle-angle_gt)),axis=0)
-        l2_distant = torch.sum(
-            (landmark_gt - landmarks) * (landmark_gt - landmarks), axis=1)
+        l2_distant = torch.sum((landmark_gt - landmarks) * (landmark_gt - landmarks), axis=1)
 
         return torch.mean(l2_distant)
