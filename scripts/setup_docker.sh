276b4f20d883102e230a3a64878c548ee855ef04
LynnL4 <lht856@foxmail.com>
2023-05-15 16:43:05 +0800

    scripts: add setup_docker.sh

--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -5,7 +5,7 @@
 INSTALL_OPTIONAL=true
 INSTALL_DOCS=false
 CUDA_AVAILABLE="$(command -v nvidia-smi)"
-PYTHON_PATH="/usr/bin/python3.8 "
+PYTHON_PATH="/usr/bin/python3.8"
 
     
 # ansi colors
@@ -14,11 +14,6 @@ GREEN='\033[032m'
 BLUE='\033[034m' 
 RST='\033[m'
 
-
-# use $1 for python path if not empty
-if [ -z "$1" ]; then
-    PYTHON_PATH="$1"
-fi
 echo -en "Using python... ${BLUE}${PYTHON_PATH}${RST}\n"
 
 # check cuda
@@ -30,6 +25,8 @@ else
     echo -en "${GREEN}OK${RST}\n"
 fi
 
+
+echo ${PYTHON_PATH}
 
 # install base deps
 echo -en "Installing base deps... "
@@ -56,19 +53,6 @@ if [ "$?" != 0 ]; then
     echo -en "OpenMIM install deps failed... ${RED}Exiting${RST}\n"
     exit 1
 fi
-
-
-# install optional deps
-if [ "${INSTALL_OPTIONAL}" ]; then
-    ${PYTHON_PATH} -m pip install -r requirements/inference.txt
-fi
-
-
-# install docs deps
-if [ "${INSTALL_DOCS}" == true ]; then
-    npm i
-fi
-
 
 echo -en "Finished setup... ${GREEN}OK${RST}\n"
 exit 0
