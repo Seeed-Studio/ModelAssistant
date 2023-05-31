d43cd666af08f4c3c34742e29d62c0cabdabf589
LynnL4 <lht856@foxmail.com>
2023-05-31 15:56:38 +0800

    fake: make it work

--- edgelab/datasets/yolodataset.py
+++ edgelab/datasets/yolodataset.py
@@ -9,7 +9,7 @@ from edgelab.registry import DATASETS
 @DATASETS.register_module()
 class CustomYOLOv5CocoDataset(YOLOv5CocoDataset):
     METAINFO = {
-        'classes': (),
+        'classes': ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'N'),
         # palette is a list of color tuples, which is used for visualization.
         'palette':
         [(220, 20, 60), (119, 11, 32), (0, 0, 142), (0, 0, 230), (106, 0, 228),
@@ -40,50 +40,50 @@ class CustomYOLOv5CocoDataset(YOLOv5CocoDataset):
                  **kwargs):
         super().__init__(*args, batch_shapes_cfg=batch_shapes_cfg, **kwargs)
 
-    def load_data_list(self) -> List[dict]:
-        """Load annotations from an annotation file named as ``self.ann_file``
+    # def load_data_list(self) -> List[dict]:
+    #     """Load annotations from an annotation file named as ``self.ann_file``
 
-        Returns:
-            List[dict]: A list of annotation.
-        """  # noqa: E501
-        with get_local_path(self.ann_file,
-                            backend_args=self.backend_args) as local_path:
-            self.coco = self.COCOAPI(local_path)
-        # The order of returned `cat_ids` will not
-        # change with the order of the `classes`
-        self.cat_ids = self.coco.get_cat_ids(
-            cat_names=self.
-            metainfo['classes'] if len(self.metainfo['classes']) else [
-                cat['name'] for cat in self.coco.dataset['categories']
-                if (cat['supercategory'] != "none")
-            ])
+    #     Returns:
+    #         List[dict]: A list of annotation.
+    #     """  # noqa: E501
+    #     with get_local_path(self.ann_file,
+    #                         backend_args=self.backend_args) as local_path:
+    #         self.coco = self.COCOAPI(local_path)
+    #     # The order of returned `cat_ids` will not
+    #     # change with the order of the `classes`
+    #     self.cat_ids = self.coco.get_cat_ids(
+    #         cat_names=self.
+    #         metainfo['classes'] if len(self.metainfo['classes']) else [
+    #             cat['name'] for cat in self.coco.dataset['categories']
+    #             if (cat['supercategory'] != "none")
+    #         ])
 
-        self.cat2label = {cat_id: i for i, cat_id in enumerate(self.cat_ids)}
-        self.cat_img_map = copy.deepcopy(self.coco.cat_img_map)
+    #     self.cat2label = {cat_id: i for i, cat_id in enumerate(self.cat_ids)}
+    #     self.cat_img_map = copy.deepcopy(self.coco.cat_img_map)
 
-        img_ids = self.coco.get_img_ids()
-        data_list = []
-        total_ann_ids = []
-        for img_id in img_ids:
-            raw_img_info = self.coco.load_imgs([img_id])[0]
-            raw_img_info['img_id'] = img_id
+    #     img_ids = self.coco.get_img_ids()
+    #     data_list = []
+    #     total_ann_ids = []
+    #     for img_id in img_ids:
+    #         raw_img_info = self.coco.load_imgs([img_id])[0]
+    #         raw_img_info['img_id'] = img_id
 
-            ann_ids = self.coco.get_ann_ids(img_ids=[img_id])
-            raw_ann_info = self.coco.load_anns(ann_ids)
-            total_ann_ids.extend(ann_ids)
+    #         ann_ids = self.coco.get_ann_ids(img_ids=[img_id])
+    #         raw_ann_info = self.coco.load_anns(ann_ids)
+    #         total_ann_ids.extend(ann_ids)
 
-            parsed_data_info = self.parse_data_info({
-                'raw_ann_info':
-                raw_ann_info,
-                'raw_img_info':
-                raw_img_info
-            })
-            data_list.append(parsed_data_info)
-        if self.ANN_ID_UNIQUE:
-            assert len(set(total_ann_ids)) == len(
-                total_ann_ids
-            ), f"Annotation ids in '{self.ann_file}' are not unique!"
+    #         parsed_data_info = self.parse_data_info({
+    #             'raw_ann_info':
+    #             raw_ann_info,
+    #             'raw_img_info':
+    #             raw_img_info
+    #         })
+    #         data_list.append(parsed_data_info)
+    #     if self.ANN_ID_UNIQUE:
+    #         assert len(set(total_ann_ids)) == len(
+    #             total_ann_ids
+    #         ), f"Annotation ids in '{self.ann_file}' are not unique!"
 
-        del self.coco
+    #     del self.coco
 
-        return data_list
+    #     return data_list
