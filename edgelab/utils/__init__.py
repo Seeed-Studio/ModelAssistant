86f928a0676ab8158b749e01c97704273e276857
mjq2020 <mjqx2011@163.com>
2023-08-16 02:54:16 +0000

    optimize:Adapt to non-development mode installation

--- edgelab/utils/__init__.py
+++ edgelab/utils/__init__.py
@@ -1,3 +1,6 @@
 from .cv import NMS, load_image, xywh2xyxy, xyxy2cocoxywh
+from .config import load_config
+from .inference import Infernce
+from .iot_camera import IoTCamera
 
-__all__ = ['NMS', 'xywh2xyxy', 'xyxy2cocoxywh', 'load_image']
+__all__ = ['NMS', 'xywh2xyxy', 'xyxy2cocoxywh', 'load_image', 'load_config', 'Infernce', 'IoTCamera']
