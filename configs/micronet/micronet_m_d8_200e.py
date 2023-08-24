94c0121af92987bf1b5dc04770e2f9b14b1e3d1d
mjq2020 <mjqx2011@163.com>
2023-08-24 11:10:26 +0000

    add: micronet config file

---
+++ configs/micronet/micronet_m_d8_200e.py
@@ -0,0 +1,21 @@
+_base_ = './base.py'
+
+gray = False
+num_classes = 10
+
+model = dict(
+    type='edgelab.ImageClassifier',
+    data_preprocessor=dict(
+        type='mmdet.DetDataPreprocessor',
+        mean=[0.0] if gray else [0.0, 0.0, 0.0],
+        std=[255.0] if gray else [255.0, 255.0, 255.0],
+    ),
+    backbone=dict(type='MicroNet', rep=False, arch='m', gray=gray),
+    head=dict(
+        type='mmcls.LinearClsHead',
+        in_channels=276,
+        num_classes=num_classes,
+        loss=dict(type='mmcls.CrossEntropyLoss', loss_weight=1.0),
+        topk=(1, 5),
+    ),
+)
