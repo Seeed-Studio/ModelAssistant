65791d11ffd4c3303394dda1bc42505fbed49334
LynnL4 <lht856@foxmail.com>
2023-08-29 17:56:00 +0800

    docs: rename the title SenseCraft to SSCMA

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,7 +5,7 @@ import zh_CN from './locales/zh_CN'
 
 export default defineConfig({
     base: '/',
-    title: 'SenseCraft',
+    title: 'SSCMA',
     lastUpdated: true,
     cleanUrls: true,
 
@@ -15,7 +15,7 @@ export default defineConfig({
         ['meta', { property: 'og:title', content: 'SSCMA' }],
         ['meta', { property: 'og:image', content: '/og-image.png' }],
         ['meta', { property: 'og:url', content: 'https://github.com/Seeed-Studio/SSCMA' }],
-        ['meta', { property: 'og:description', content: 'SenSeCraft Model Assiant is an open-source project focused on embedded AI.' }],
+        ['meta', { property: 'og:description', content: 'Seeed SenSeCraft Model Assiant is an open-source project focused on embedded AI.' }],
         ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
         ['meta', { name: 'twitter:site', content: '@seeedstudio' }],
         ['meta', { name: 'theme-color', content: '#051726' }]
