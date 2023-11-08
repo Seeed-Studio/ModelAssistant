c223e5f94d39151a261423996389338cb0cac7dc
mjq2020 <mjqx2011@163.com>
2023-11-08 10:57:28 +0000

    add: semisup loss

--- sscma/models/losses/__init__.py
+++ sscma/models/losses/__init__.py
@@ -2,5 +2,14 @@ from .bce_withlogits_loss import BCEWithLogitsLoss
 from .classfication_loss import LabelSmoothCrossEntropyLoss
 from .nll_loss import NLLLoss
 from .pfld_loss import PFLDLoss
+from .domain_focal_loss import DomainFocalLoss, TargetLoss, DomainLoss
 
-__all__ = ['LabelSmoothCrossEntropyLoss', 'PFLDLoss', 'NLLLoss', 'BCEWithLogitsLoss']
+__all__ = [
+    'LabelSmoothCrossEntropyLoss',
+    'PFLDLoss',
+    'NLLLoss',
+    'BCEWithLogitsLoss',
+    'DomainFocalLoss',
+    'TargetLoss',
+    'DomainLoss',
+]
