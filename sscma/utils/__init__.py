0ee1b7a3479e6578277e575d17a13f75a7897dfe
mjq2020 <mjqx2011@163.com>
2023-11-09 08:05:21 +0000

    Add: the function of installing and checking third-party libraries

--- sscma/utils/__init__.py
+++ sscma/utils/__init__.py
@@ -2,5 +2,17 @@ from .cv import NMS, load_image, xywh2xyxy, xyxy2cocoxywh
 from .config import load_config
 from .inference import Infernce
 from .iot_camera import IoTCamera
+from .check import net_online, install_lib, check_lib
 
-__all__ = ['NMS', 'xywh2xyxy', 'xyxy2cocoxywh', 'load_image', 'load_config', 'Infernce', 'IoTCamera']
+__all__ = [
+    'NMS',
+    'xywh2xyxy',
+    'xyxy2cocoxywh',
+    'load_image',
+    'load_config',
+    'Infernce',
+    'IoTCamera',
+    'net_online',
+    'install_lib',
+    'check_lib',
+]
---
+++ sscma/utils/check.py
@@ -0,0 +1,66 @@
+from typing import Optional, Union, Iterable
+import subprocess
+
+
+def net_online() -> bool:
+    """
+    Check whether the current device is connected to the Internet.
+    """
+    import socket
+
+    for host in '1.1.1.1', '8.8.8.8', '223.5.5.5':  # Cloudflare, Google, AliDNS:
+        try:
+            test_connection = socket.create_connection(address=(host, 53), timeout=2)
+        except (socket.timeout, socket.gaierror, OSError):
+            continue
+        else:
+            # If the connection was successful, close it to avoid a ResourceWarning
+            test_connection.close()
+            return True
+    return False
+
+
+def install_lib(name: Union[str, Iterable[str]], version: Optional[Union[str, Iterable[str]]] = None) -> bool:
+    """Install python third-party libraries."""
+    if isinstance(name, str):
+        name = [name]
+    if version is not None:
+        if isinstance(version, str):
+            name = [i + version for i in name]
+        else:
+            name = [i + version[idx] for idx, i in enumerate(name)]
+
+    if version is not None:
+        name = '=='.join(name)
+    try:
+        assert net_online(), "Network Connection Failure!"
+        print(f"Third-party library {name} being installed")
+        subprocess.check_output(f'pip install --no-cache {name}', shell=True).decode()
+        return True
+    except Exception as e:
+        print(f"Warning ⚠️: Installation of {name} has failed, the installation process has been skipped")
+        return False
+
+
+def check_lib(name: str, version: Optional[str] = None, install: bool = True) -> bool:
+    """
+    Check if the third-party libraries have been installed.
+    """
+    import pkg_resources as pkg
+
+    flag = True
+    try:
+        pkg.require(name)
+    except pkg.DistributionNotFound:
+        try:
+            import importlib
+
+            importlib.import_module(next(pkg.parse_requirements(name)).name)
+        except ImportError:
+            flag = False
+    except pkg.VersionConflict:
+        pass
+
+    if install and not flag:
+        return install_lib(name, version=version)
+    return flag
