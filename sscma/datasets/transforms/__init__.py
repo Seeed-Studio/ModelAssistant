83aadccac4ed67d9025f47fe24e55ca18b39fbda
nullptr <nullptr@localhost>
2024-05-31 06:50:44 +0000

    chore: register modified modules

--- sscma/datasets/transforms/__init__.py
+++ sscma/datasets/transforms/__init__.py
@@ -1,5 +1,20 @@
 from .formatting import PackSensorInputs
-from .loading import LoadSensorFromFile
+from .loading import LoadSensorFromFile, YOLOLoadAnnotations
 from .wrappers import MutiBranchPipe
+from .mosaic import Mosaic
+from .color import YOLOv5HSVRandomAug
+from .affine import YOLOv5RandomAffine
+from .resize import YOLOv5KeepRatioResize, LetterResize
 
-__all__ = ['PackSensorInputs', 'LoadSensorFromFile', 'MutiBranchPipe']
+
+__all__ = [
+    'PackSensorInputs',
+    'LoadSensorFromFile',
+    'MutiBranchPipe',
+    'Mosaic',
+    'YOLOLoadAnnotations',
+    'YOLOv5HSVRandomAug',
+    'YOLOv5RandomAffine',
+    'YOLOv5KeepRatioResize',
+    'LetterResize',
+]
