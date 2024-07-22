eadc4767508a87b5b8d2b0b5c4ff225906e6e0c1
Baozhu Zuo <zuobaozhu@gmail.com>
2024-07-22 11:23:31 +0800

    configs: use TimmClassifier for test

--- configs/models/timm_resnet50.py
+++ configs/models/timm_resnet50.py
@@ -1,18 +1,28 @@
 # model settings
+data_preprocessor = dict(
+    num_classes=1000,
+    # RGB format normalization parameters
+    mean=[123.675, 116.28, 103.53],
+    std=[58.395, 57.12, 57.375],
+    # convert image from BGR to RGB
+    to_rgb=True,
+)
+
+
+
 model = dict(
-        type='ImageClassifier',
-        backbone=dict(
-            type='TimmClassifier',
-            model_name='resnet50',
-            loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
-            pretrained=True),
-        neck=dict(type='GlobalAveragePooling'),
-        head=dict(
-                type='LinearClsHead',
-                num_classes=1000,
-                in_channels=2048,
-                loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
-                topk=(1, 5),
-            )
-        )
+    data_preprocessor=data_preprocessor,
+    type="TimmClassifier",
+    model_name="resnet50",
+    pretrained=True,
+    num_classes=1000,
+    loss=dict(
+        type="CrossEntropyLoss",
+        loss_weight=1.0,
+    ),
+    train_cfg=dict(augments=[
+        dict(type='Mixup', alpha=0.8),
+        dict(type='CutMix', alpha=1.0)
+    ])
+)
 
