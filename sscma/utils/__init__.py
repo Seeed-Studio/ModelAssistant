8b68cdc16816f06ea20eb59acd923f5fda5a749d
--unset <baozhu.zuo@gmail.com>
2024-07-24 16:17:23 +0800

    move all the CV-related operations into simplecv

---
+++ sscma/utils/__init__.py
@@ -0,0 +1,5 @@
+from .simplecv import simplecv_imresize, simplecv_imflip,simplecv_imcrop,simplecv_imrescale, \
+						simplecv_imread, simplecv_imfrombytes,simplecv_imwrite
+
+__all__ = ['simplecv_imresize','simplecv_imflip','simplecv_imcrop','simplecv_imrescale',
+		   'simplecv_imread', 'simplecv_imfrombytes','simplecv_imwrite']
