a305b9299316c75e7a0580052d35e770e7a7eb24
Baozhu Zuo <baozhu.zuo@gmail.com>
2024-08-16 11:07:46 +0800

    test: update dastset test py

--- tests/dataset_tests.py
+++ tests/dataset_tests.py
@@ -2,20 +2,20 @@ import torch
 from torch.utils.data import DataLoader
 import tqdm
 
-
 from sscma.datasets.coco import CocoDataset, coco_collate
 
 from sscma.datasets.transforms.formatting import PackDetInputs
 from sscma.datasets.transforms.processing import RandomResize
 from sscma.datasets.transforms.loading import LoadAnnotations, LoadImageFromFile
 from sscma.datasets.transforms.transforms import (
-    CachedMixUp,
-    CachedMosaic,
+    MixUp,
+    Mosaic,
     Pad,
     RandomCrop,
     RandomFlip,
     Resize,
-    YOLOXHSVRandomAug,
+    HSVRandomAug,
+    toTensor,
 )
 
 from mmengine.dataset.sampler import DefaultSampler
@@ -30,7 +30,7 @@ if __name__ == "__main__":
 
     print(f"\ntorch version: {torch.__version__}")
 
-    for pin_memory in [False, True]:
+    for pin_memory in [True, False]:
         for numpy in [True]:
 
             train_pipeline = [
@@ -40,26 +40,26 @@ if __name__ == "__main__":
                     backend_args=None,
                 ),
                 dict(type=LoadAnnotations, imdecode_backend="pillow", with_bbox=True),
-                # dict(type=CachedMosaic, img_scale=(640, 640), pad_val=114.0),
-                # dict(
-                #     type=RandomResize,
-                #     scale=(1280, 1280),
-                #     ratio_range=(0.1, 2.0),
-                #     resize_type=Resize,
-                #     keep_ratio=True,
-                # ),
-                # dict(type=RandomCrop, crop_size=(640, 640)),
-                # dict(type=YOLOXHSVRandomAug),
-                # dict(type=RandomFlip, prob=0.5),
-                # dict(type=Pad, size=(640, 640), pad_val=dict(img=(114, 114, 114))),
-                # dict(
-                #     type=CachedMixUp,
-                #     img_scale=(640, 640),
-                #     ratio_range=(1.0, 1.0),
-                #     max_cached_images=20,
-                #     pad_val=(114, 114, 114),
-                # ),
-                dict(type=Resize, scale=(224, 224), keep_ratio=False),
+                dict(type=HSVRandomAug),
+                dict(type=toTensor),
+                dict(type=Mosaic, img_scale=(640, 640), pad_val=114.0),
+                dict(
+                    type=RandomResize,
+                    scale=(1280, 1280),
+                    ratio_range=(0.1, 2.0),
+                    resize_type=Resize,
+                    keep_ratio=True,
+                ),
+                dict(type=RandomCrop, crop_size=(640, 640)),
+                dict(type=RandomFlip, prob=0.5),
+                dict(type=Pad, size=(640, 640), pad_val=dict(img=(114, 114, 114))),
+                dict(
+                    type=MixUp,
+                    img_scale=(640, 640),
+                    ratio_range=(1.0, 1.0),
+                    max_cached_images=20,
+                    pad_val=114.0,
+                ),
                 dict(type=PackDetInputs),
             ]
 
@@ -81,7 +81,7 @@ if __name__ == "__main__":
 
             print("\n" + "-" * 20 + f"\n{numpy=} {pin_memory=} \n")
 
-            for workers in range(8, 13, 2):
+            for workers in range(16, 25, 2):
                 loader = DataLoader(
                     dataset=dataset,
                     num_workers=workers,
