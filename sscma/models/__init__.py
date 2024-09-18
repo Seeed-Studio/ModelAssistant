992c431b3408204b073d04b9d89a1ee82705d712
mjq2020 <mjqx2011@163.com>
2024-09-18 06:39:33 +0000

    add: init import

--- sscma/models/__init__.py
+++ sscma/models/__init__.py
@@ -1,9 +1,14 @@
-from .model import ImageClassifier
-from .batch_augments import Mixup, CutMix
 from .backbones import *  # noqa: F401,F403
+from .classifiers import *  # noqa: F401,F403
+from .cnn import *  # noqa: F401,F403
+from .detectors import *  # noqa: F401,F403
 from .heads import *  # noqa: F401,F403
+from .layers import *  # noqa: F401,F403
 from .losses import *  # noqa: F401,F403
 from .necks import *  # noqa: F401,F403
+from .task_modules import *  # noqa: F401,F403
+from .test_time_augs import *  # noqa: F401,F403
+from .utils import *  # noqa: F401,F403
 
 
 __all__ = ["ImageClassifier", "Mixup", "CutMix"]
---
+++ sscma/models/utils/__init__.py
@@ -0,0 +1,4 @@
+from .batch_augments import Mixup, CutMix
+from .data_processor import ClsDataPreprocessor
+
+__all__ = ["Mixup", "CutMix", "ClsDataPreprocessor"]
