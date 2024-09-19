f6db085687577d613c63bfca4dacefc68553228c
mjq2020 <mjqx2011@163.com>
2024-09-19 10:04:43 +0000

    format: code with black

--- tools/train.py
+++ tools/train.py
@@ -2,60 +2,61 @@ import argparse
 import logging
 import os
 import os.path as osp
-# import torch.multiprocessing as mp
-# mp.set_start_method("forkserver", force=True)
-from multiprocessing import set_start_method
-set_start_method("spawn",force=True)
 import torch
-#训练python脚本中import torch后，加上下面这句。 
-torch.multiprocessing.set_sharing_strategy('file_system')
-import mmpretrain.datasets.transforms
-# import torch
+
+torch.multiprocessing.set_sharing_strategy("file_system")
+
 from mmengine.config import Config, DictAction
 from mmengine.logging import print_log
 from mmengine.registry import RUNNERS
 from mmengine.runner import Runner
-# torch.backends.cuda.matmul.allow_tf32 = True
-# torch.backends.cudnn.enabled = True
-# torch.backends.cudnn.benchmark = False
-# torch.backends.cudnn.allow_tf32 = True
+
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Train a detector')
-    parser.add_argument('config', help='train config file path')
-    parser.add_argument('--work-dir', help='the dir to save logs and models')
+    parser = argparse.ArgumentParser(description="Train a detector")
+    parser.add_argument("config", help="train config file path")
+    # parser.add_argument('--work-dir', help='the dir to save logs and models')
     parser.add_argument(
-        '--amp',
-        action='store_true',
+        "--amp",
+        action="store_true",
         default=False,
-        help='enable automatic-mixed-precision training')
+        help="enable automatic-mixed-precision training",
+    )
     parser.add_argument(
-        '--auto-scale-lr',
-        action='store_true',
-        help='enable automatically scaling LR.')
+        "--auto-scale-lr", action="store_true", help="enable automatically scaling LR."
+    )
     parser.add_argument(
-        '--resume',
-        action='store_true',
-        help='resume from the latest checkpoint in the work_dir automatically')
+        "--resume",
+        action="store_true",
+        help="resume from the latest checkpoint in the work_dir automatically",
+    )
     parser.add_argument(
-        '--cfg-options',
-        nargs='+',
+        "--work_dir",
+        "--work-dir",
+        default="work_dirs",
+        help="the dir to save logs and models",
+    )
+    parser.add_argument(
+        "--cfg-options",
+        nargs="+",
         action=DictAction,
-        help='override some settings in the used config, the key-value pair '
-        'in xxx=yyy format will be merged into config file. If the value to '
+        help="override some settings in the used config, the key-value pair "
+        "in xxx=yyy format will be merged into config file. If the value to "
         'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
         'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
-        'Note that the quotation marks are necessary and that no white space '
-        'is allowed.')
+        "Note that the quotation marks are necessary and that no white space "
+        "is allowed.",
+    )
     parser.add_argument(
-        '--launcher',
-        choices=['none', 'pytorch', 'slurm', 'mpi'],
-        default='none',
-        help='job launcher')
-    parser.add_argument('--local_rank','--local-rank', type=int, default=0)
+        "--launcher",
+        choices=["none", "pytorch", "slurm", "mpi"],
+        default="none",
+        help="job launcher",
+    )
+    parser.add_argument("--local_rank", "--local-rank", type=int, default=0)
     args = parser.parse_args()
-    if 'LOCAL_RANK' not in os.environ:
-        os.environ['LOCAL_RANK'] = str(args.local_rank)
+    if "LOCAL_RANK" not in os.environ:
+        os.environ["LOCAL_RANK"] = str(args.local_rank)
 
     return args
 
@@ -73,42 +74,49 @@ def main():
     if args.work_dir is not None:
         # update configs according to CLI args if args.work_dir is not None
         cfg.work_dir = args.work_dir
-    elif cfg.get('work_dir', None) is None:
+    elif cfg.get("work_dir", None) is None:
         # use config filename as default work_dir if cfg.work_dir is None
-        cfg.work_dir = osp.join('./work_dirs',
-                                osp.splitext(osp.basename(args.config))[0])
+        cfg.work_dir = osp.join(
+            "./work_dirs", osp.splitext(osp.basename(args.config))[0]
+        )
 
     # enable automatic-mixed-precision training
     if args.amp is True:
         optim_wrapper = cfg.optim_wrapper.type
-        if optim_wrapper == 'AmpOptimWrapper':
+        if optim_wrapper == "AmpOptimWrapper":
             print_log(
-                'AMP training is already enabled in your config.',
-                logger='current',
-                level=logging.WARNING)
+                "AMP training is already enabled in your config.",
+                logger="current",
+                level=logging.WARNING,
+            )
         else:
-            assert optim_wrapper == 'OptimWrapper', (
-                '`--amp` is only supported when the optimizer wrapper type is '
-                f'`OptimWrapper` but got {optim_wrapper}.')
-            cfg.optim_wrapper.type = 'AmpOptimWrapper'
-            cfg.optim_wrapper.loss_scale = 'dynamic'
+            assert optim_wrapper == "OptimWrapper", (
+                "`--amp` is only supported when the optimizer wrapper type is "
+                f"`OptimWrapper` but got {optim_wrapper}."
+            )
+            cfg.optim_wrapper.type = "AmpOptimWrapper"
+            cfg.optim_wrapper.loss_scale = "dynamic"
 
     # enable automatically scaling LR
     if args.auto_scale_lr:
-        if 'auto_scale_lr' in cfg and \
-                'enable' in cfg.auto_scale_lr and \
-                'base_batch_size' in cfg.auto_scale_lr:
+        if (
+            "auto_scale_lr" in cfg
+            and "enable" in cfg.auto_scale_lr
+            and "base_batch_size" in cfg.auto_scale_lr
+        ):
             cfg.auto_scale_lr.enable = True
         else:
-            raise RuntimeError('Can not find "auto_scale_lr" or '
-                               '"auto_scale_lr.enable" or '
-                               '"auto_scale_lr.base_batch_size" in your'
-                               ' configuration file.')
+            raise RuntimeError(
+                'Can not find "auto_scale_lr" or '
+                '"auto_scale_lr.enable" or '
+                '"auto_scale_lr.base_batch_size" in your'
+                " configuration file."
+            )
 
     # cfg.resume = args.resume
 
     # build the runner from config
-    if 'runner_type' not in cfg:
+    if "runner_type" not in cfg:
         # build the default runner
         runner = Runner.from_cfg(cfg)
     else:
@@ -120,5 +128,5 @@ def main():
     runner.train()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
