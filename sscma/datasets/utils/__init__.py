771ac6a49e32936243b1df912a5252cba98b0ca5
mjq2020 <mjqx2011@163.com>
2024-10-25 11:17:53 +0000

    add: dataset utils

--- sscma/datasets/utils/__init__.py
+++ sscma/datasets/utils/__init__.py
@@ -1,3 +1,4 @@
 from .sampler import LanceDistributedSampler
+from .parse_pose import parse_pose_metainfo
 
-__all__ = ["LanceDistributedSampler"]
+__all__ = ["LanceDistributedSampler", "parse_pose_metainfo"]
