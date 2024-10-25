136c73501d1e4150d99ec09338b2d0b26ea77636
mjq2020 <mjqx2011@163.com>
2024-10-25 11:14:54 +0000

    add: add model utils

--- sscma/models/utils/__init__.py
+++ sscma/models/utils/__init__.py
@@ -1,4 +1,22 @@
 from .batch_augments import Mixup, CutMix
 from .data_processor import ClsDataPreprocessor
+from .misc import (
+    samplelist_boxtype2tensor,
+    make_divisible,
+    make_round,
+    auto_arrange_images,
+    get_file_list,
+    IMG_EXTENSIONS,
+)
 
-__all__ = ["Mixup", "CutMix", "ClsDataPreprocessor"]
+__all__ = [
+    "Mixup",
+    "CutMix",
+    "ClsDataPreprocessor",
+    "samplelist_boxtype2tensor",
+    "make_divisible",
+    "make_round",
+    "auto_arrange_images",
+    "get_file_list",
+    "IMG_EXTENSIONS",
+]
