7815974c069e61696905ad0f57979c7f1b05a044
Baozhu Zuo <baozhu.zuo@gmail.com>
2024-12-02 18:06:23 +0800

    sscma: add QuantizerSwitchHook

--- sscma/engine/hooks/__init__.py
+++ sscma/engine/hooks/__init__.py
@@ -1,5 +1,6 @@
 from .memory_profiler_hook import MemoryProfilerHook
 from .pipeline_switch_hook import PipelineSwitchHook
 from .visualization_hook import DetVisualizationHook
+from .quantizer_switch_hook import QuantizerSwitchHook
 
-__all__ = ["MemoryProfilerHook", "PipelineSwitchHook", "DetVisualizationHook"]
+__all__ = ["MemoryProfilerHook", "PipelineSwitchHook", "DetVisualizationHook","QuantizerSwitchHook"]
---
+++ sscma/engine/hooks/quantizer_switch_hook.py
@@ -0,0 +1,30 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch
+from mmengine.hooks import Hook
+
+
+class QuantizerSwitchHook(Hook):
+    """Switch data pipeline at switch_epoch.
+
+    Args:
+        freeze_quantizer_epoch (int): the epoch to freeze the quantizer.
+        freeze_bn_epoch (int): the epoch to freeze the batch normalization
+            statistics.
+    """
+
+    def __init__(self,freeze_quantizer_epoch, freeze_bn_epoch):
+        self.freeze_quantizer_epoch = freeze_quantizer_epoch
+        self.freeze_bn_epoch = freeze_bn_epoch
+
+
+    def before_train_epoch(self, runner):
+        """switch pipeline."""
+        epoch = runner.epoch
+        #if epoch == runner.max_epoch // 3:
+        if epoch == self.freeze_quantizer_epoch:
+            runner.logger.info("freeze quantizer parameters")
+            runner.model.apply(torch.quantization.disable_observer)
+        #elif epoch == runner.max_epoch // 3 * 2:
+        elif epoch == self.freeze_bn_epoch:
+            runner.logger.info("freeze batch norm mean and variance estimates")
+            runner.model.apply(torch.nn.intrinsic.qat.freeze_bn_stats)
