0e6067d5915a26494745eecb7c1315a4daa0ee2b
mjq2020 <mjqx2011@163.com>
2024-12-17 09:17:53 +0000

    modify: checkpoint save file

--- mmengine/runner/runner.py
+++ mmengine/runner/runner.py
@@ -384,6 +384,7 @@ class Runner:
         else:
             self._experiment_name = self.timestamp
         self._log_dir = osp.join(self.work_dir, self.timestamp)
+        self._work_dir = self._log_dir
         mmengine.mkdir_or_exist(self._log_dir)
         # Used to reset registries location. See :meth:`Registry.build` for
         # more details.
