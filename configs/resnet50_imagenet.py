6489e4ac252939b18da60052f60f2342ec6993bc
Baozhu Zuo <baozhu.zuo@gmail.com>
2024-12-19 10:29:09 +0800

    configs: Separate the pretrain and cls dir

--- configs/resnet50_imagenet.py
+++
@@ -1,9 +0,0 @@
-from mmengine.config import read_base
-
-with read_base():
-    from .datasets.imagenet_bs32 import *
-    from ._base_.default_runtime import *
-    from .models.timm_classify import *
-    from .schedules.sgd_linear_coslr_bs2048 import *
-
-model.model_name = "resnet50"
